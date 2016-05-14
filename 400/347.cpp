e1ff409e531f83bd7d70633e94da19ba1c102fac
Fan Wu <wufan@udel.edu>
2016-05-14 11:54:50 -0400

    20160514

--- 400/347.cpp
+++ 400/347.cpp
@@ -1,3 +1,26 @@
+/*
+  Given a non-empty array of integers, return the k most frequent elements.
+
+  For example,
+  Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
+  Note: 
+  You may assume k is always valid, 1 ¡Ü k ¡Ü number of unique elements.
+  Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+  Solution:
+  To solve this problem, we first need to count the frequency of every element of the array.
+  Then, group the entry-freqeuency map by using the Bucket sort.
+  The result comes from the last k elements of the bucket sort array.
+
+  Note: 
+  You should know that some buckets are empty in the bucket sequence. 
+  Skip them when to read the elements of soem given frequency.
+
+  Tags:
+  Count, Bucket sort.
+*/
+
 #include <iostream>
 #include <unordered_map>
 #include <vector>
