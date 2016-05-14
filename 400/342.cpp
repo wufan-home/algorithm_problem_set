7a8224e42cb55dfe31d55f154c5bcd1c26215c01
Fan Wu <wufan@udel.edu>
2016-05-14 11:43:14 -0400

    20160514

--- 400/342.cpp
+++ 400/342.cpp
@@ -1,3 +1,15 @@
+/*
+  Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+  Example:
+  Given num = 16, return true. Given num = 5, return false.
+  
+  Follow up: Could you solve it without loops/recursion?
+
+  Solution:
+  A inumber is a power of 4 if and only if it is greater than 0, even, a power of two and its value minus 1 is divisible by 3. 
+*/
+
 #include <iostream>
 
 using namespace std;
