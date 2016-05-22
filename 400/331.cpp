20bd4d5df89ad7c5a5b7b1e63c1939ead9b9a6c4
flyingmath33 <deepexploring@gmail.com>
2016-05-21 22:33:22 -0400

    Clean the code 331.cpp

--- 400/331.cpp
+++ 400/331.cpp
@@ -1,5 +1,7 @@
 /*
-  One way to serialize a binary tree is to use pre-order traversal. When we encounter a non-null node, we record the node's value. If it is a null node, we record using a sentinel value such as #.
+  One way to serialize a binary tree is to use pre-order traversal. 
+  When we encounter a non-null node, we record the node's value. 
+  If it is a null node, we record using a sentinel value such as #.
 
      _9_
     /   \
@@ -9,25 +11,32 @@
 / \ / \   / \
 # # # #   # #
 
-For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where # represents a null node.
+  For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", 
+  where # represents a null node.
 
-Given a string of comma separated values, verify whether it is a correct preorder traversal serialization of a binary tree. Find an algorithm without reconstructing the tree.
+  Given a string of comma separated values, 
+  verify whether it is a correct preorder traversal serialization of a binary tree. 
+  Find an algorithm without reconstructing the tree.
 
-Each comma separated value in the string must be either an integer or a character '#' representing null pointer.
+  Each comma separated value in the string must be either an integer or a character '#' representing null pointer.
 
-You may assume that the input format is always valid, for example it could never contain two consecutive commas such as "1,,3".
+  You may assume that the input format is always valid, 
+  for example it could never contain two consecutive commas such as "1,,3".
 
-Example 1:
-"9,3,4,#,#,1,#,#,2,#,6,#,#"
-Return true
+  Example 1:
+  "9,3,4,#,#,1,#,#,2,#,6,#,#"
+  Return true
 
-Example 2:
-"1,#"
-Return false
+  Example 2:
+  "1,#"
+  Return false
 
-Example 3:
-"9,#,#,1"
-Return false
+  Example 3:
+  "9,#,#,1"
+  Return false
+
+  Solution:
+  DFS.
 */
 
 #include <iostream>
@@ -77,12 +86,4 @@ bool isValidSerialization(string preorder) {
 	}
 
 	return vi == size;  // More values than real needed.
-}
-
-int main()
-{
-	cout << isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#") << endl;
-	cout << isValidSerialization("1,#") << endl;
-	cout << isValidSerialization("1") << endl;
-	return 1;
 }
