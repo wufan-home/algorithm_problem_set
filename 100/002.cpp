b968c75bd7f1369090f8b325972de51e72de0e38
flyingmath33 <deepexploring@gmail.com>
2016-06-11 15:38:21 -0400

    Add Two Numbers

--- 100/002.cpp
+++ 100/002.cpp
@@ -1,4 +1,23 @@
-#include <iostream>
+/*
+  You are given two linked lists representing two non-negative numbers. 
+  The digits are stored in reverse order and each of their nodes contain a single digit. 
+  Add the two numbers and return it as a linked list.
+  Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+  Output: 7 -> 0 -> 8
+
+  Solution: Let two pointers, each of which runs from one head of two linked-lists,
+  run until both of them are NULL. 
+  Build a new node by the sum of two values from the two linked-lists.
+  In the end, add a new node if the overflow is 1 after the loop.
+*/
+
+#include <climits>
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+};
 
 using namespace std;
 
