fccd681892f47419a478822e1d9b2f842b72927b
flyingmath33 <deepexploring@gmail.com>
2016-06-12 16:22:26 -0400

    Flatten Binary Tree to Linked List

--- 200/114.cpp
+++ 200/114.cpp
@@ -27,4 +27,15 @@ Hints:
 If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
 */
 
+void flatten(TreeNode* root) {
+        if(root == NULL)
+		return;
 
+	TreeNode *right = root->right;
+	root->right = root->left;
+	root->left = NULL;
+	TreeNode *cur = root;
+	for(; cur->right; cur = cur->right) {}
+	cur->right = right;
+	flatten(root->right);
+}
