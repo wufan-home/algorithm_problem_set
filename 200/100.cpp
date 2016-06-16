ac48d6c7cf3c5d5aa8b01fdc3c2c863796299308
flyingmath33 <deepexploring@gmail.com>
2016-06-15 23:52:41 -0400

    Same Tree

--- 200/100.cpp
+++ 200/100.cpp
@@ -4,4 +4,11 @@
   if they are structurally identical and the nodes have the same value.
 */
 
-
+bool isSameTree(TreeNode* p, TreeNode* q) {
+	return ((p == NULL && q == NULL) ||
+		(p != NULL &&
+		 q != NULL &&
+		 p->val == q->val &&
+		 isSameTree(p->left, q->left) &&
+		 isSameTree(p->right, q->right)));
+}
