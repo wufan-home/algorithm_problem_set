d9870628fc973806bc8655a2d4b9fbaedaa89a26
flyingmath33 <deepexploring@gmail.com>
2016-06-17 20:17:33 -0400

    Linked List Cycle II

---
+++ 200/142.cpp
@@ -0,0 +1,34 @@
+/*
+  Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+  Note: Do not modify the linked list.
+  Follow up:
+  Can you solve it without using extra space?
+*/
+
+ListNode *detectCycle(ListNode *head) {
+	if(head == NULL)
+		return NULL;
+
+	ListNode *slow = head;
+	ListNode *fast = head;
+	do
+	{
+		slow = slow->next;
+		fast = fast->next;
+		if(fast)
+			fast = fast->next;
+	}while(slow && fast && slow != fast);
+
+	if(fast == NULL && slow == NULL)
+		return NULL;
+
+	fast = head;
+	do
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+	while(slow != fast);
+
+	return fast;
+}
