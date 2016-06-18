b2e792bbe9be6c9b1083bb0142fba12dedc67642
flyingmath33 <deepexploring@gmail.com>
2016-06-18 12:27:54 -0400

    Happy Number

--- 300/202.cpp
+++ 300/202.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+/*
+  Write an algorithm to determine if a number is "happy".
+  A happy number is a number defined by the following process: 
+  Starting with any positive integer, replace the number by the sum of the squares of its digits, 
+  and repeat the process until the number equals 1 (where it will stay), 
+  or it loops endlessly in a cycle which does not include 1. 
+  Those numbers for which this process ends in 1 are happy numbers.
+  Example: 19 is a happy number
+  12 + 92 = 82
+  82 + 22 = 68
+  62 + 82 = 100
+  12 + 02 + 02 = 1
 
-using namespace std;
+  Solution:
+  If the number contains only one digit, e.g. n = d, the checking function will run
+  as d^2 + 0^2.
+
+  It is easy to see that n = 2 .. 6, n is not a happy number.
+  Then, we can return false, when the sum of squares is in this range.
+*/
 
 bool isHappy(int n) {
         while(n >= 7)
@@ -12,9 +29,4 @@ bool isHappy(int n) {
 	}
 
 	return n == 1;
-}
-
-int main()
-{
-	return 1;
 }
