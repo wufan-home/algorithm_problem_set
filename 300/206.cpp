b01e0d29acadebac98ff97a900bfbd7210a7bc71
flyingmath33 <deepexploring@gmail.com>
2016-06-18 13:56:50 -0400

    Reverse Linked List

--- 300/206.cpp
+++ 300/206.cpp
@@ -1,71 +1,48 @@
-// 206.cpp : Defines the entry point for the console application.
-//
+/*
+  Reverse a singly linked list.
+  A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
 
-#include "stdafx.h"
-#include <vector>
-#include <utility>
-#include <unordered_set>
-#include <iostream>
-#include <stack>
-
-using namespace std;
-
-bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
-	if (numCourses == 0 || prerequisites.empty())
-		return true;
-
-	const int& n = numCourses;
-	const vector<pair<int, int>>& p = prerequisites;
-
-	vector<vector<int>> deps(n, vector<int>(n, 0));
-	for (int i = 0; i < p.size(); ++i)
-	{
-		deps[p[i].first][p[i].second] = 1;
-		++deps[p[i].second][p[i].second];
-	}
-
-	vector<int> schedule;
-	for (int i = 0; i < n; ++i)
-	{
-		if (deps[i][i] == 0)
-		{
-			unordered_set<int> visited;
-			stack<int> node_stack;
-			node_stack.push(i);
-			while (!node_stack.empty())
-			{
-				int cur = node_stack.top();
-				node_stack.pop();
-				for (int j = 0; j < n; ++j)
-				{
-					if (j == cur || !deps[cur][j])
-						continue;
-					if (visited.find(j) != visited.end())
-						return false;
-					if (find(schedule.begin(), schedule.end(), j) == schedule.end())
-					{
-						visited.insert(j);
-						schedule.push_back(j);
-						node_stack.push(j);
-					}
-				}
-				visited.insert(cur);
-				schedule.push_back(cur);
-			}
-		}
-	}
-
-	return schedule.size() == n;
+ListNode* reverseList(ListNode* head) {
+        ListNode *reverse = new ListNode(INT_MIN);
+        while(head)
+        {
+            ListNode *temp = reverse->next;
+            reverse->next = head;
+            head = head->next;
+            reverse->next->next = temp;
+        }
+        return reverse->next;
 }
 
-int main()
+ListNode *Reverse(ListNode *head, ListNode* &tail)
 {
-	vector<pair<int, int>> pres(1, make_pair(0, 1));
-	pres.push_back(make_pair(1, 2));
-	pres.push_back(make_pair(2, 3));
-	pres.push_back(make_pair(3, 0));
-	pres.push_back(make_pair(4, 3));
-	cout << canFinish(5, pres) << endl;
-    return 0;
-}
+    	if(head == NULL)
+    		return NULL;
+    
+    	ListNode *temp = head;
+    	head = head->next;
+    	temp->next = NULL;
+    	ListNode *reverse = Reverse(head, tail);
+    	if(reverse == NULL)
+    	{
+    	    reverse = temp;
+    	    tail = temp;
+    	}
+    	else
+    	{
+    	    tail->next = temp;
+    		tail = tail->next;
+    	}
 
+    	return reverse;
+}
+    
+ListNode* reverseList(ListNode* head)
+{
+    	if(head == NULL)
+    		return NULL;
+    
+        ListNode *tail = NULL;
+    	return Reverse(head, tail);
+}
