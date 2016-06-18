d26f3e22c0e45a70be5a7f8d08bdd72f1ce03fe2
flyingmath33 <deepexploring@gmail.com>
2016-06-18 12:35:33 -0400

    Remove Linked List Elements

--- 300/203.cpp
+++ 300/203.cpp
@@ -1,27 +1,21 @@
-#include <iostream>
-#include <climits>
+/*
+  Remove all elements from a linked list of integers that have value val.
+  Example
+  Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+  Return: 1 --> 2 --> 3 --> 4 --> 5
+*/
 
-using namespace std;
-
-struct ListNode
-{
-	int val;
-	ListNode *next;
-};
 
 ListNode* removeElements(ListNode* head, int val) {
 	ListNode *dummy = new ListNode(INT_MIN);
-    	ListNode *cur = dummy;
-    	while(head)
-    	{
-    		ListNode *cur_ = head;
-    		head = head->next;
-    		if(cur_->val != val)
-    		{
-    			cur->next = cur_;
-    			cur = cur->next;
-    		}
-    	}
-    	cur->next = NULL;
-    	return dummy->next;
+        dummy->next = head;
+        for(ListNode *cur = dummy; cur->next;)
+        {
+            if(cur->next->val == val)
+                cur->next = cur->next->next;
+            else
+                cur = cur->next;
+        }
+        
+        return dummy->next;
 }
