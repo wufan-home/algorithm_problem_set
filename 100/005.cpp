17d44636fefeba2a0a3a4397eb2ab9e6d7034717
Fan Wu <deepexploring@gmail.com>
2016-06-23 22:18:41 -0400

    Update 005.cpp

--- 100/005.cpp
+++ 100/005.cpp
@@ -1,14 +1,28 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
+/*
+	Given a string S, find the longest palindromic substring in S. 
+	You may assume that the maximum length of S is 1000, 
+	and there exists one unique longest palindromic substring.
+*/
 
 string longestPalindrome(string s) {
-        vector<int> length(s.size(), 1);
-}
-
-int main()
-{
-	return 1;
+        if(s.size() <= 1)
+            return s;
+            
+        int start = 0;
+        int max_len = 1;
+        for(int i = 0; i < s.size() - max_len / 2; )
+        {
+            int l = i;
+            int r = i;
+            for(; r < s.size() - 1 && s[r + 1] == s[r]; ++r) {}
+            i = r + 1;
+            for(; l > 0 && r < s.size() - 1 && s[l - 1] == s[r + 1]; --l, ++r) {}
+            if(r - l + 1 > max_len)
+            {
+                max_len = r - l + 1;
+                start = l;
+            }
+        }
+        
+        return s.substr(start, max_len);
 }
