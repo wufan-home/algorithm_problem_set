25721a7d991aeb69429bda829eeb65a2d84c46cf
flyingmath33 <deepexploring@gmail.com>
2016-06-24 21:37:29 -0400

    Reverse Integer

--- 100/007.cpp
+++ 100/007.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
-#include <climits>
+/*
+  Reverse digits of an integer.
+  Example1: x = 123, return 321
+  Example2: x = -123, return -321
 
-using namespace std;
+  Have you thought about this?
+  Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
+  If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
+  Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, 
+  then the reverse of 1000000003 overflows. How should you handle such cases?
+  For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+  Update (2014-11-10):
+  Test cases had been added to test the overflow behavior.
+
+  Solution: For such problems, we first remove the sign if needed.
+*/
 
 int reverse(int x) {
 	long long rv = 0;
