50bebf8f2bc1f6ebf77f66aa32eb2392ddaa707b
flyingmath33 <deepexploring@gmail.com>
2016-06-24 22:06:21 -0400

    Container With Most Water

--- 100/011.cpp
+++ 100/011.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+/*
+  Given n non-negative integers a1, a2, ..., an, 
+  where each represents a point at coordinate (i, ai). 
+  n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+  Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+  Note: You may not slant the container.
 
-using namespace std;
+  Solution: Greedy method. Since the height is determined by the min value of two vertical lines, 
+  the height won't increase if we move the longer vertical line. The width always decreases, then
+  it implies better solution, i.e., a larger container, can only be obtained by moving the shorter vertical line.
+*/
 
 int maxArea(vector<int>& height) {
         if(height.size() < 2)
@@ -18,11 +24,4 @@ int maxArea(vector<int>& height) {
 
 	return max_area;
 }
-
-int main()
-{
-	vector<int> h(1, 2);
-	h.push_back(1);
-	cout << maxArea(h) << endl;
-	return 1;
-}
+ 
