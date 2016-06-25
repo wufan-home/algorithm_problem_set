8b61547d7480034e63f7096fa79a0554d81a2ed8
flyingmath33 <deepexploring@gmail.com>
2016-06-24 23:38:14 -0400

    3Sum

--- 100/015.cpp
+++ 100/015.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
+/*
+  Given an array S of n integers, are there elements a, b, c in S 
+  such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+  Note: The solution set must not contain duplicate triplets.
+  For example, given array S = [-1, 0, 1, 2, -1, -4],
+  A solution set is:
+  [
+    [-1, 0, 1],
+    [-1, -1, 2]
+  ]
+*/
 
 vector<vector<int> > threeSum(vector<int>& nums) {
   vector<vector<int> > results;
@@ -38,21 +44,4 @@ vector<vector<int> > threeSum(vector<int>& nums) {
   }
 
   return results;
-}
-
-int main()
-{
-  int array[] = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
-
-  vector<int> nums(array, array + sizeof(array) / sizeof(int));
-
-  vector<vector<int> > results = threeSum(nums);
-  cout << results.size() << endl;
-  for(int i = 0; i < results.size(); ++i) {
-    for(int j = 0; j < results[i].size(); ++j) {
-      cout << results[i][j] << ", ";
-    }
-    cout << endl;
-  }
-  return 1;
 }
