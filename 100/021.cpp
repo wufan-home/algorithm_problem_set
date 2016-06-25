90179bb0d87b9a5ae38d7fe82af61c337ac72626
flyingmath33 <deepexploring@gmail.com>
2016-06-25 13:04:22 -0400

    Merge Two Sorted Lists

--- 100/021.cpp
+++ 100/021.cpp
@@ -1,23 +1,19 @@
-#include <iostream>
+/*
+  Merge two sorted linked lists and return it as a new list. 
+  The new list should be made by splicing together the nodes of the first two lists.
 
-using namespace std;
+  Solution: Use a reference to a pointer to avoid the determine which pointer of the list
+  should be moved.
+*/
 
 ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         ListNode *dummy = new ListNode(INT_MIN);
-	ListNode *cur = dummy;
-	while(l1 != NULL || l2 != NULL)
-	{
-		ListNode *next = ((l1 == NULL ? INT_MAX : l1->val) <=
-				  (l2 == NULL ? INT_MAX : l2->val) ?
-				  l1 : l2);
-		cur->next = next;
-		cur = cur->next;
-		next == l1 ? l1 = l1->next : l2 = l2->next;
-	}
-	return dummy->next;
-}
-
-int main()
-{
-	return 1;
+        for(ListNode *cur = dummy; l1 || l2;)
+        {
+            ListNode* &moving = ((l1 ? l1->val : INT_MAX) <= (l2 ? l2->val : INT_MAX)) ? l1 : l2;
+            cur->next = moving;
+            moving = moving->next;
+            cur = cur->next;
+        }
+        return dummy->next;
 }
