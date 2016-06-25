52d3f8c904aa28fc95cef052ceafe93268bf6df8
flyingmath33 <deepexploring@gmail.com>
2016-06-25 14:05:59 -0400

    Generate Parentheses

--- 100/022.cpp
+++ 100/022.cpp
@@ -1,47 +1,46 @@
-#include <string>
-#include <vector>
-#include <iostream>
+/*
+  Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+  For example, given n = 3, a solution set is:
+  [
+    "((()))",
+    "(()())",
+    "(())()",
+    "()(())",
+    "()()()"
+  ]
 
-using namespace std;
+  Solution: This is a recursion with two parameters, i.e., one is the num of left parenthesises,
+  and the other one is the number of the right parenthesises. 
+  If the two params are all equal to the number of patenthesis, the string is a valid one and put it into the result collection. 
+  If the 1st param is larger than the given size, we got an invalid candidate and return without record it.
+  Otherwise, notice that the 2nd param must be always smaller than the 1st one.
+  Loop by adding ')' to the candidate string until the two params are equal and call the recursion function.
 
-void GenerateAux(int left, int right, int n, string str, vector<string>& rv)
+  Use an index cur to determine the writing poision.
+*/
+
+void Generate(string& result, int cur, int l_count, int r_count, int n, vector<string>& results)
 {
-	if(right == n)
-	{
-		rv.push_back(str);
-		return;
-	}
-
-	if(left < n)
-	{
-		str.push_back('(');
-		for(int i = 0; right + i <= left + 1; ++i)
-		{
-			GenerateAux(left + 1, right + i, n, str, rv);
-			str.push_back(')');
-		}
-	}
+        if(cur == result.size())
+            results.push_back(result);
+        else if(l_count < n)
+        {
+            result[cur++] = '(';
+	    ++l_count;
+            for(; r_count <= l_count; ++r_count)
+            {
+                Generate(result, cur, l_count, r_count, n, results);
+                result[cur++] = ')';
+            }
+        }
 }
-
+    
 vector<string> generateParenthesis(int n) {
-	vector<string> rv;
-	if(n == 0)
-		return rv;
-
-	string str("");
-	GenerateAux(0, 0, n, str, rv);
-	return rv;
-}
-
-int main()
-{
-	for(int i = 3; i <= 3; ++i)
-	{
-		cout << "Case " << i << ": " << endl;
-		vector<string> rv = generateParenthesis(i);
-		for(int i = 0; i < rv.size(); ++i)
-			cout << rv[i] << endl;
-	}
-
-	return 1;
+        vector<string> results;
+        if(n == 0)
+            return results;
+            
+        string result(2 * n, '\0');
+        Generate(result, 0, 0, 0, n, results);
+        return results;
 }
