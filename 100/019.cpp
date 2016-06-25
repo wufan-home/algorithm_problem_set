71f1b562ee9c4181cfc9cebd1b350bcd429f6ec9
flyingmath33 <deepexploring@gmail.com>
2016-06-25 12:44:01 -0400

    Remove Nth Node From End of List

--- 100/019.cpp
+++ 100/019.cpp
@@ -1,28 +1,27 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
+/*
+  Given a linked list, remove the nth node from the end of list and return its head.
+  For example,
+  Given linked list: 1->2->3->4->5, and n = 2.
+  After removing the second node from the end, the linked list becomes 1->2->3->5.
+  Note:
+  Given n will always be valid.
+  Try to do this in one pass.
+*/
 
 ListNode* removeNthFromEnd(ListNode* head, int n) {
-	ListNode *temp_head = new ListNode(INT_MIN);
-	temp_head->next = head;
-
-	Listnode *back = temp_head;
-	ListNode *front = head;
-        for(int i = 0; i < n; ++i)
-		front = front->next;
-
-	while(front != NULL)
-	{
-		back = back->next;
-		front = front->next;
-	}
-
-	back->next = back->next->next;
-	return temp_head->next;
-}
-
-int main()
-{
-	return 1;
+	if(head == NULL)
+            return head;
+            
+        ListNode *dummy = new ListNode(INT_MIN);
+        dummy->next = head;
+        ListNode *front = dummy->next;
+        for(int i = 0; i < n; ++i, front = front->next) {}
+        ListNode *back = dummy;
+        while(front)
+        {
+            back = back->next;
+            front = front->next;
+        }
+        back->next = back->next->next;
+        return dummy->next;
 }
