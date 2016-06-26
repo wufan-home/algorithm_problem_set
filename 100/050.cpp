3f1e101fdf58f8f987f0b295c1c7526434d662c4
flyingmath33 <deepexploring@gmail.com>
2016-06-26 12:26:30 -0400

    Pow(x, n)

--- 100/050.cpp
+++ 100/050.cpp
@@ -6,6 +6,9 @@
         ((x^2)^(n/2)), if n is even.
 
   Since n/2 = 0 if n = 1 and x^0 = 1, the corner cases of 1 and -1 are covered by the above recursion.
+
+  Since n may be negative, do not use n%2 == 1. This will generate incorrect result because of the mask code.
+  The bit-operation has more scenarioes to apply.
 */
 
 double myPow(double x, int n)
@@ -15,7 +18,7 @@ double myPow(double x, int n)
     	
     	if(n == 0)
     		return 1;
-        else if (n & 1 == 1)
+        else if (n & 1 == 1) //Since n may be negative, do not use n%2 == 1.
     		return myPow(x * x, (n + (n < 0 ? 1 : -1)) / 2) * (n > 0 ? x : 1/x);
     	else
     		return myPow(x * x, n / 2);
