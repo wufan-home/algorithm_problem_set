383f0ebe1cacacd982b34690d1f73eddde3c621a
flyingmath33 <deepexploring@gmail.com>
2016-06-26 12:18:49 -0400

    Pow(x, n)

--- 100/050.cpp
+++ 100/050.cpp
@@ -1,43 +1,18 @@
-// 50.cpp : Defines the entry point for the console application.
-//
+/*
+  Implement pow(x, n).
 
-#include "stdafx.h"
-#include <iostream>
-#include <cmath>
-
-using namespace std;
-
-double MyPowAux(double x, long long n)
-{
-	if (n == 1)
-		return x;
-
-	return MyPowAux(x * x, n / 2)  * (n % 2 ? x : 1);
-}
+  Solution: The basic idea of the solution is: x^n = ((x^2)^(n/2))(*x when n > 0 or *x^(-1) when n < 0).
+*/
 
 double myPow(double x, int n)
 {
-	if (n == 0)
-		return 1;
-
-	if (x == 0  || x == 1 || n == 1)
-		return x;
-
-	bool neg = (x < 0);
-	x = fabs(x);
-	long long index = n;
-	if (index < 0)
-	{
-		index = -1 * index;
-		x = 1 / x;
-	}
-
-	return (neg ? -1 : 1) * MyPowAux(x, index);
+    	if(x == 0)
+    		return 0;
+    	
+    	if(n == 0)
+    		return 1;
+        else if (n & 1 == 1)
+    		return myPow(x * x, (n + (n < 0 ? 1 : -1)) / 2) * (n > 0 ? x : 1/x);
+    	else
+    		return myPow(x * x, n / 2);
 }
-
-int main()
-{
-	cout << myPow(2.000000, INT_MIN) << endl;
-    return 0;
-}
-
