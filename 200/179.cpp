1d75645b9b125252d4a5948bfbf3a5ef99329683
flyingmath33 <deepexploring@gmail.com>
2016-07-10 16:09:12 -0400

    Largest Number

--- 200/179.cpp
+++ 200/179.cpp
@@ -1,82 +1,47 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+/*
+  Given a list of non negative integers, arrange them such that they form the largest number.
+  For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+  Note: The result may be very large, so you need to return a string instead of an integer.
 
-using namespace std;
+  Solution: The most difficult point of this problem is to design the compare function.
+  We say number 1 will be placed after the number 2 iff the string num_1 + num_2 is smaller than
+  the string num_2 + num_1.
+*/
 
-bool Compare(const string& l, const string& r)
+bool Compare(const string& str1, const string& str2)
 {
-	static int count = 0;
-	int len = (l.size() >= r.size() ? l.size() : r.size());
-	cout << ++count << endl;
-	cout << l << ", " << r << endl;
-	for(int i = 0; i < len; ++i)
-	{
-		if(i < l.size() && i < r.size())
+        string first = str1 + str2;
+        string second = str2 + str1;
+        for(int i = 0; i < first.size(); ++i)
+        {
+		if(first[i] != second[i])
 		{
-			if(l[i] != r[i])
-				return l[i] < r[i];
+			if(first[i] >= second[i])
+				return true;
+			else
+				break;
 		}
-		else if(i >= l.size() && i < r.size())
-		{
-			if(l[l.size() - 1] != r[i])
-				return l[l.size() - 1] < r[i];
-		}
-		else if(i >= r.size() && i < l.size())
-		{
-			if(l[i] != r[r.size() - 1])
-				return l[i] < r[r.size() - 1];
-		}
-	}
-	cout << count << endl;
-	return true;
+        }
+        
+        return false;
 }
 
-string to_string(int num)
+string largestNumber(vector<int> &num)
 {
-	if(num == 0)
-		return "0";
-
-	string r("");
-	while(num != 0)
-	{
-		int digit = num % 10;
-		num /= 10;
-		r.insert(r.begin(), digit + '0');
-	}
-	return r;
-}
-
-string largestNumber(vector<int>& nums) {
-        if(nums.empty())
+        if(num.empty())
 		return "";
-
-	string r("");
-
-	int size = nums.size();
-	vector<string> snums;
-	for(int i = 0; i < size; ++i)
-		snums.push_back(to_string(nums[i]));
-	for(int i = 0; i < snums.size(); ++i)
-		cout << snums[i] << " - ";
-	cout << endl;
+            
+        vector<string> strs(num.size(), "");
+        for(int i = 0; i < num.size(); ++i)
+		strs[i] = to_string(num[i]);
 	
-	sort(snums.begin(), snums.end(), Compare);
-	for(int i = 0; i < snums.size(); ++i)
-		cout << snums[i] << " - ";
-		cout << endl;
-
-	for(int i = snums.size() - 1; i >= 0; --i)
-		r.append(snums[i]);
-
-	return r;
-}
-
-int main()
-{
-	int a[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	vector<int> nums(a, a + sizeof(a) / sizeof(int));
-	cout << largestNumber(nums) << endl;
-	return 1;
+        sort(strs.begin(), strs.end(), Compare);
+        string result("");
+        for(int i = 0; i < strs.size(); ++i)
+		result += strs[i];
+            
+        while(result[0] == '0' && result.size() > 1)
+		result.erase(result.begin());
+            
+        return result;
 }
