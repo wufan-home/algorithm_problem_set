eeb4633b00561aec137ab4de6d7c3240765ba9f1
flyingmath33 <deepexploring@gmail.com>
2016-07-17 14:21:20 -0400

    Word Ladder

--- 200/127.cpp
+++ 200/127.cpp
@@ -12,50 +12,59 @@
   return its length 5.
 */
 
-
+int GetMinimalPath(const string& start, const string& end, unordered_map<string, vector<string> >& adj)
+{
+        int min_len = INT_MAX;
+        for(int i = 0; i < adj[start].size(); ++i)
+        {
+		if(adj[start][i] == end)
+		{
+			min_len = 2;
+			break;
+		}
+		else
+		{
+			int len = GetMinimalPath(adj[start][i], end, adj);
+			min_len = min(min_len, len == INT_MAX ? len : 1 + len);
+		}
+        }
+        return min_len;
+}
+    
 int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList)
 {
-	if (beginWord.empty() || endWord.empty() || wordList.empty())
+        wordList.insert(endWord);
+    	wordList.erase(beginWord);
+    	if(wordList.empty())
 		return 0;
 
-	if (beginWord == endWord)
-		return 1;
-
-	int len = 1;
-	queue<string> word_queue;
-	word_queue.push(beginWord);
-	int count = 1;
-	int size = wordList.begin()->size();
-	wordList.insert(endWord);
-	while (word_queue.size())
-	{
+    	int len = beginWord.size();
+    	unordered_map<string, vector<string> > adj;
+    	queue<string> word_queue;
+    	word_queue.push(beginWord);
+    	while(!word_queue.empty())
+    	{
 		string cur = word_queue.front();
 		word_queue.pop();
-		--count;
-
-		if (cur == endWord)
-    		return len;
-
-		for(int i = 0; i < size; ++i)
+		adj[cur] = vector<string>();
+    	    
+		for(int i = 0; i < len; ++i)
 		{
-		    string temp = cur;
-		    for(char l = 'a'; l <= 'z'; ++l)
-		    {
-		        temp[i] = l;
-		        if(wordList.find(temp) != wordList.end())
-		        {
-		            word_queue.push(temp);
-		            wordList.erase(temp);
-		        }
-		    }
+			string s = cur;
+			for(char ch = 'a'; ch <= 'z'; ++ch)
+			{
+				cur[i] = ch;
+				if(wordList.find(cur) != wordList.end())
+				{
+					adj[s].push_back(cur);
+					word_queue.push(cur);
+					wordList.erase(cur);
+				}
+			}
+			cur = s;
 		}
-		
-		if(!count)
-		{
-		    ++len;
-		    count = word_queue.size();
-		}
-	}
-
-	return 0;
+    	}
+    	
+    	int min_len = GetMinimalPath(beginWord, endWord, adj);
+    	return min_len == INT_MAX ? 0 : min_len;
 }
