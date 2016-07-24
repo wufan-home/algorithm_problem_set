a12c47f99544ded43887a423c1b6cddf025d4605
flyingmath33 <deepexploring@gmail.com>
2016-07-24 16:06:23 -0400

    Search in Rotated Sorted Array

--- 100/033.cpp
+++ 100/033.cpp
@@ -1,69 +1,35 @@
-#include <iostream>
-#include <vector>
+/*
+  Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+  (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+  You are given a target value to search. If found in the array return its index, otherwise return -1.
+  You may assume no duplicate exists in the array.
 
-using namespace std;
+  Solution: Binary search. We first distinguish which part is a rotate array.
+  For the sorted part, use the usual binary search.
+  For the rotated part, use the recursive part.
+*/
 
 int Search(const vector<int>& nums, int target)
 {
-  if(nums.empty())
-    return -1;
-  
-  if(nums.size() == 1)
-    return (nums[0] == target ? 0 : -1);
-
-
-  int start = 0;
-  int end = nums.size() - 1;
-  while(start <= end) {
-    //cout << "Start = " << start << ", end = " << end;
-    if(start == end)
-      return nums[start] == target ? start : -1;
-
-    if(start == end - 1) {
-      if(target == nums[start])
-	return start;
-      else if(target == nums[end])
-	return end;
-      else
-	return -1;
-    }
-
-    int mid = (start + end + 1) / 2;
-    //cout << ", mid = " << mid << endl;
-    if(target == nums[mid])
-      return mid;
-    else if(nums[start] < nums[mid]) {
-      //right side
-      if(nums[start] <= target && target < nums[mid])
-	end = mid - 1;
-      else
-	start = mid + 1;
-    } else if(nums[start] > nums[mid]) {
-      //left side
-      if(nums[mid] < target && target <= nums[end])
-	start = mid + 1;
-      else
-	end = mid - 1;
-    }
-  }
-
-  return -1;
-}
-
-int main()
-{
-  vector<int> nums1;
-  for(int i = 7; i < 8; ++i)
-    nums1.push_back(i);
-  for(int i = 0; i < 7; ++i)
-    nums1.push_back(i);
-
-  for(int i = 0; i < nums1.size(); ++i)
-    cout << nums1[i] << ", ";
-  cout << endl;
-
-  for(int i = -1; i <= 11; ++i)
-    cout << "target = " << i << ": " << Search(nums1, i) << endl;
-
-  return 1;
+	int l = 0;
+	int r = nums.size() - 1;
+	while(l < r)
+	{
+		int mid = l + (r + 1 - l) / 2;
+		if(nums[mid] <= nums[r])
+		{
+			if(nums[mid] <= target && target <= nums[r])
+				l = mid;
+			else
+				r = mid - 1;
+		}
+		else
+		{
+			if(nums[l] <= target && target <= nums[mid])
+				r = mid;
+			else
+				l = mid + 1;
+		}
+	}
+	return nums.empty() || nums[l] != target ? -1 : l;
 }
