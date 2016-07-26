85a3a56e4f6892ac3d1e58ea440e1655ecf42f3d
flyingmath33 <deepexploring@gmail.com>
2016-07-25 23:16:25 -0400

    Search for a Range

--- 100/034.cpp
+++ 100/034.cpp
@@ -1,56 +1,54 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
+/*
+  Given a sorted array of integers, find the starting and ending position of a given target value.
+  Your algorithm's runtime complexity must be in the order of O(log n).
+  If the target is not found in the array, return [-1, -1].
+  For example,
+  Given [5, 7, 7, 8, 8, 10] and target value 8,
+  return [3, 4].
+*/
 
 vector<int> searchRange(vector<int>& nums, int target) {
-	vector<int> rv;
-	if(nums.empty())
-		return rv;
-
-	int exist = -1;
-	int start = 0;
-	int end = nums.size() - 1;
-	for(; start <= end;)
-	{
-		int mid = start + (end - start + 1) / 2;
-		if(nums[mid] == target)
-		{
-			exist = mid;
-			break;
-		}
-		else if(mid == end)
-		{
-			cout << nums[start] << endl;
-			exist = (nums[start] == target ? start : -1);
-			break;
-		}
-		else if(nums[mid] < target)
-			start = mid;
-		else
-			end = mid;
-	}
-
-	if(exist > -1)
-	{
-		for(start = exist - 1; start >= 0 && nums[start] == nums[exist]; --start) {}
-		rv.push_back(start + 1);
-		for(end = exist + 1; end < nums.size() && nums[end] == nums[exist]; ++end) {}
-		rv.push_back(end - 1);
-	}
-	else
-	{
-		rv.push_back(-1);
-		rv.push_back(-1);
-	}
-
-	return rv;
+        vector<int> result;
+        vector<int>::iterator lb = lower_bound(nums.begin(), nums.end(), target);
+        result.push_back(nums.empty() || lb == nums.end() || *lb != target ? -1 : lb - nums.begin());
+        vector<int>::iterator ub = upper_bound(nums.begin(), nums.end(), target);
+        result.push_back(nums.empty() || ub == nums.begin() || *(ub - 1) != target ? -1 : ub - nums.begin() - 1);
+        return result;
 }
 
-int main()
+int lower(const vector<int>& nums, int target)
 {
-	vector<int> nums({1, 2, 3});
-	vector<int> rv = searchRange(nums, 1);
-	cout << rv[0] << ", " << rv[1] << endl;
-	return 1;
+	int l = 0;
+	int r = nums.size() - 1;
+	while(l < r)
+	{
+		int mid = l + (r - l) / 2;
+		if(target <= nums[mid])
+			r = mid;
+		else
+			l = mid + 1;
+	}
+	return nums.empty() || l == nums.size() || nums[l] != target ? -1 : l;
+}
+
+int upper(const vector<int>& nums, int target)
+{
+	int l = 0;
+	int r = nums.size() - 1;
+	while(l < r)
+	{
+		int mid = l + (r + 1 - l) / 2;
+		if(nums[mid] <= target)
+			l = mid;
+		else
+			r = mid - 1;
+	}
+	return nums.empty() || l == nums.size() || nums[l] != target ? -1 : l;
+}
+
+vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> result;
+        result.push_back(lower(nums, target));
+	result.push_back(upper(nums, target));
+	return result;
 }
