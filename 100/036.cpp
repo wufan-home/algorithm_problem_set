9835515e7f9a3af075545fea1c282a4f4b00f966
flyingmath33 <deepexploring@gmail.com>
2016-07-27 21:17:50 -0400

    Valid Sudoku

--- 100/036.cpp
+++ 100/036.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <bitset>
-
-using namespace std;
+/*
+  Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+  The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+  A partially filled sudoku which is valid.
+  Note:
+  A valid Sudoku board (partially filled) is not necessarily solvable. 
+  Only the filled cells need to be validated.
+*/
 
 bool isValidSudoku(vector<vector<char> >& board) {
         if(board.size() != 9 || board[0].size() != 9)
@@ -20,14 +23,7 @@ bool isValidSudoku(vector<vector<char> >& board) {
 
 			int p = board[i][j] - '0';
 			if(row_test[i][p] || col_test[j][p] || block_test[3 * (i / 3) + j / 3][p])
-			{
-				cout << i << ", " << j << endl;
-				cout << board[i][j] << endl;
-				cout << row_test[i][p] << endl;
-				cout << col_test[j][p] << endl;
-				cout << block_test[3 * (i / 3) + j / 3][p] << endl;
 				return false;
-			}
 
 			row_test[i][p] = true;
 			col_test[j][p] = true;
@@ -36,37 +32,4 @@ bool isValidSudoku(vector<vector<char> >& board) {
 	}
 
 	return true;
-}
-
-int main()
-{
-//	["..5.....6","....14...",".........",".....92..","5....2...",".......3.","...54....","3.....42.","...27.6.."]
-	vector<vector<char> > board;
-	for(int i = 0; i < 9; ++i)
-	{
-		board.push_back(vector<char>());
-		for(int j = 0; j < 9; ++j)
-			board[i].push_back('.');
-	}
-
-	board[0][2] = '5';
-	board[0][8] = '6';
-	board[1][4] = '1';
-	board[1][5] = '4';
-	board[3][5] = '9';
-	board[3][6] = '2';
-	board[4][0] = '5';
-	board[4][5] = '2';
-	board[5][7] = '3';
-	board[6][3] = '5';
-	board[6][4] = '4';
-	board[7][0] = '3';
-	board[7][6] = '4';
-	board[7][7] = '2';
-	board[8][3] = '2';
-	board[8][4] = '7';
-	board[8][6] = '6';
-	cout << isValidSudoku(board) << endl;
-	
-	return 1;
 }
