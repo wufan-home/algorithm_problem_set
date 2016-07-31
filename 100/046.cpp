7a5c8e13a53f6a5e082ac412b3ab9b19c10700a5
flyingmath33 <deepexploring@gmail.com>
2016-07-31 15:47:36 -0400

    Permutations0

--- 100/046.cpp
+++ 100/046.cpp
@@ -1,57 +1,28 @@
 /*
-   Given a collection of distinct numbers, return all possible permutations.
+  Given a collection of distinct numbers, return all possible permutations.
 
-   For example,
-   [1,2,3] have the following permutations:
-   [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1]. 
+  For example,
+  [1,2,3] have the following permutations:
+  [1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1]. 
 */
 
-#include <vector>
-#include <iostream>
-
-using namespace std;
-
-vector<vector<int> > permute(vector<int>& nums) {
-	vector<vector<vector<int> > > perms_alt(2, vector<vector<int> >());
-	if(nums.empty())
-		return perms_alt[0];
-
-	vector<int> perm(1, nums[0]);
-	perms_alt[0].push_back(perm);
-	if(nums.size() == 1)
-		return perms_alt[0];
-
-	for(int i = 1; i < nums.size(); ++i)
-	{
-		int current = i % 2;
-		int last = (i - 1) % 2;
-		for(int j = perms_alt[last].size() - 1; j >= 0; --j)
-		{
-			vector<int> perm = perms_alt[last][j];
-			perm.push_back(nums[i]);
-			perms_alt[current].push_back(perm);
-			for(int l = perm.size() - 1; l > 0; --l)
-			{
-				swap(perm[l], perm[l - 1]);
-				perms_alt[current].push_back(perm);
-			}
-		}
-		perms_alt[last].clear();
-	}
-	
-	return perms_alt[(nums.size() - 1) % 2];
-}
-
-int main()
+void DoPermute(vector<int>& nums, int cur, vector<vector<int>>& permutes)
 {
-	int a[] = { 1, 2, 3};
-	vector<int> nums(a, a + sizeof(a) / sizeof(int));
-	vector<vector<int> > r = permute(nums);
-	for(int j = 0; j < r.size(); ++j)
+        if(cur == nums.size() - 1)
+		permutes.push_back(nums);
+        else
+        {
+		for(int i = cur; i < nums.size(); ++i)
 		{
-			for(int l = 0; l < r[j].size(); ++l)
-				cout << r[j][l] << ", ";
-			cout << endl;
+			swap(nums[i], nums[cur]);
+			DoPermute(nums, cur + 1, permutes);
+			swap(nums[i], nums[cur]);
 		}
-	return 1;
+        }
+}
+    
+vector<vector<int> > permute(vector<int>& nums) {
+        vector<vector<int>> permutes;
+        DoPermute(nums, 0, permutes);
+        return permutes;
 }
