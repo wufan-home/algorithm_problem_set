708d3c7602d50308290de6902906b81825419b44
Fan Wu <deepexploring@gmail.com>
2016-10-19 01:43:43 -0400

    Read N Characters Given Read4

--- 200/157.cpp
+++ 200/157.cpp
@@ -3,7 +3,7 @@
 		The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
 		By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
 		Note:
-		The read function will only be called once for each test case.
+		The read function will only be called once for each test case
 	*/
 
 	int read(char *buf, int n) {
