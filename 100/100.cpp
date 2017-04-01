53de137e18696e6cdc01886fb80c74d88c39b031
Fan Wu <deepexploring@gmail.com>
2017-04-01 15:23:06 -0700

    Same Tree

--- 100/100.cpp
+++ 100/100.cpp
@@ -5,10 +5,7 @@
 */
 
 bool isSameTree(TreeNode* p, TreeNode* q) {
-	return ((p == NULL && q == NULL) ||
-		(p != NULL &&
-		 q != NULL &&
-		 p->val == q->val &&
-		 isSameTree(p->left, q->left) &&
-		 isSameTree(p->right, q->right)));
+	return (p == NULL && q == NULL) ||
+	    (p != NULL && q != NULL && p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
 }
+
