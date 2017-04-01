02272b00506927ee3a10b4b9aea9d23911bc4e71
Fan Wu <deepexploring@gmail.com>
2017-04-01 15:51:14 -0700

    Same Tree

--- 100/100.cpp
+++ 100/100.cpp
@@ -5,7 +5,39 @@
 */
 
 bool isSameTree(TreeNode* p, TreeNode* q) {
+	// Recursive: DFS
 	return (p == NULL && q == NULL) ||
 	    (p != NULL && q != NULL && p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
 }
 
+bool isSameTree(TreeNode* p, TreeNode* q) {
+	//BFS
+        queue<TreeNode *> l_queue;
+        queue<TreeNode *> r_queue;
+        
+        l_queue.push(p);
+        r_queue.push(q);
+            
+        while(!l_queue.empty() && !r_queue.empty())
+        {
+            TreeNode *l_ptr = l_queue.front();
+            l_queue.pop();
+            
+            TreeNode *r_ptr = r_queue.front();
+            r_queue.pop();
+            
+            if(l_ptr == NULL && r_ptr == NULL)
+                continue;
+            else if(l_ptr == NULL ||
+                    r_ptr == NULL ||
+                    (l_ptr->val != r_ptr->val))
+                return false;
+
+            l_queue.push(l_ptr->left);
+            l_queue.push(l_ptr->right);
+            r_queue.push(r_ptr->left);
+            r_queue.push(r_ptr->right);
+        }
+        
+        return l_queue.empty() && r_queue.empty();
+}
