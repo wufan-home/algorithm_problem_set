360fbcac05c580273d948ea51c44fa133905418a
Fan Wu <deepexploring@gmail.com>
2017-04-02 19:20:46 -0700

    Binary Tree Zigzag Level Order Traversal

--- 200/103.cpp
+++ 200/103.cpp
@@ -1,50 +1,63 @@
-#include <iostream>
-#include <queue>
-#include <vector>
+/*
+	Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
 
-using namespace std;
+	For example:
+	Given binary tree [3,9,20,null,null,15,7],
+	    3
+	   / \
+	  9  20
+	    /  \
+	   15   7
+	return its zigzag level order traversal as:
+	[
+	  [3],
+	  [20,9],
+	  [15,7]
+	]
+*/
 
-struct TreeNode
-{
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-};
 
-vector<vector<int> > zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int> > rv;
-	if(root == NULL)
-		return rv;
-
-	TreeNode *p_cur = NULL;
-	queue<TreeNode *> node_queue;
-	node_queue.push(root);	
-	int level_size = 0;
-	int level_number = -1;
-	while(!node_queue.empty())
-	{
-		if(level_size == 0)
-		{
-			level_size = node_queue.size();
-			rv.push_back(vector<int>());
-			++level_number;
-		}
-		
-		p_cur = node_queue.front();
-		node_queue.pop();
-		--level_size;
-
-		if(level_number % 2 == 1)
-			rv[level_number].insert(rv[level_number].begin(), p_cur->val);
-		else
-			rv[level_number].push_back(p_cur->val);
-
-		if(p_cur->left)
-			node_queue.push(p_cur->left);
-
-		if(p_cur->right)
-			node_queue.push(p_cur->right);
-	}
-
-	return rv;
-}
+vector<vector<int>> zigzagLevelOrder(TreeNode* root) 
+    {
+        vector<vector<int>> levelValuesZigzag;
+        
+        if(root == NULL)
+            return levelValuesZigzag;
+            
+        queue<TreeNode *> queueForTree;
+        queueForTree.push(NULL);
+        queueForTree.push(root);
+        bool forwardPushValue = false;
+        
+        for(int curLevel = -1; ; )
+        {
+            const TreeNode *curNode = queueForTree.front();
+            queueForTree.pop();
+            
+            if(curNode == NULL)
+            {
+                if(queueForTree.empty())
+                    break;
+                    
+                levelValuesZigzag.push_back(vector<int>());
+                ++curLevel;
+                forwardPushValue = !forwardPushValue;
+                queueForTree.push(NULL);
+            }
+            else
+            {
+                if(forwardPushValue)
+                    levelValuesZigzag[curLevel].push_back(curNode->val);
+                else
+                    levelValuesZigzag[curLevel].insert(levelValuesZigzag[curLevel].begin(), 1, curNode->val);
+                    
+                if(curNode->left)
+                    queueForTree.push(curNode->left);
+                    
+                if(curNode->right)
+                    queueForTree.push(curNode->right);
+            }
+        }
+        
+        return levelValuesZigzag;
+    }
