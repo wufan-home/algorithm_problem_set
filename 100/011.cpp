9fe737ff67ef8f70d4aa08cfc579bff33be6fb28
Fan Wu <deepexploring@gmail.com>
2017-04-08 14:47:05 -0700

    Container With Most Water

--- 100/011.cpp
+++ 100/011.cpp
@@ -12,16 +12,23 @@
 
 int maxArea(vector<int>& height) {
         if(height.size() < 2)
-		return 0;
-
-	int max_area = 0;
-	for(int s = 0, e = height.size() - 1; s < e;)
-	{
-		int area = (e - s) * min(height[s], height[e]);
-		max_area = max(area, max_area);
-		height[s] <= height[e] ? ++s : --e;
-	}
-
-	return max_area;
-}
+            return 0;
+            
+        int maxAreaValue = 0;
+        for(int l = 0, r = height.size() - 1; l < r;)
+        {
+            maxAreaValue = max(maxAreaValue, min(height[l], height[r]) * (r - l));
+            if(height[l] > height[r])
+                --r;
+            else if(height[l] < height[r])
+                ++l;
+            else
+            {
+                ++l;
+                --r;
+            }
+        }
+        
+        return maxAreaValue;
+    }
  
