e33d02e5818df7ec53840f83655b787f60a9dba8
Fan Wu <deepexploring@gmail.com>
2017-04-09 13:56:01 -0700

    Longest Common Prefix

--- 100/014.cpp
+++ 100/014.cpp
@@ -3,24 +3,23 @@
 */
 
 
-string longestCommonPrefix(vector<string>& strs) {
-	string prefix("");
-
-	if(strs.empty())
-		return prefix;
-
-	prefix = strs[0];
-	for(int i = 1; i < strs.size(); ++i)
-	{
-		for(int j = 0; j < prefix.size(); ++j)
-		{
-			if(prefix[j] != strs[i][j])
-			{
-				prefix = prefix.substr(0, j);
-				break;
-			}
-		}
-	}
-
-	return prefix;
-}
+ string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())
+            return "";
+            
+        string prefix = strs[0];
+        int prefix_end_index = strs[0].size() - 1;
+        for(int i = 1; i < strs.size(); ++i)
+        {
+            for(int j = 0; j <= prefix_end_index; ++j)
+            {
+                if(strs[i][j] != strs[0][j])
+                {
+                    prefix_end_index = j - 1;
+                    break;
+                }
+            }
+        }
+        
+        return strs[0].substr(0, prefix_end_index + 1);
+    }
