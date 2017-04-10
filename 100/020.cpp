07047e2846eeeed6037a4e3068976a74372b2a11
Fan Wu <deepexploring@gmail.com>
2017-04-09 17:22:52 -0700

    Valid Parentheses

--- 100/020.cpp
+++ 100/020.cpp
@@ -4,47 +4,30 @@
   The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 */
 
-bool isValid(string s) {
-        if(s.empty() || s.size() % 2 != 0)
-		return false;
-
-	unordered_map<char, int> op_to_int;
-	op_to_int['('] = 1;
-	op_to_int[')'] = -1;
-	op_to_int['['] = 2;
-	op_to_int[']'] = -2;
-	op_to_int['{'] = 3;
-	op_to_int['}'] = -3;
-	stack<int> op_stack;
-	for(int i = 0; i < s.size(); ++i)
-	{
-		int weight = op_to_int[s[i]];
-		if(weight > 0)
-			op_stack.push(weight);
-		else
-		{
-			if(op_stack.empty() || weight + op_stack.top() != 0)
-				return false;
-
-			op_stack.pop();
-		}
-	}
-
-	return op_stack.empty();
-}
-
-bool isValid1(string s) {
-        stack<char> op_stack;
+class Solution {
+public:
+    bool isValid(string s) {
         for(int i = 0; i < s.size(); ++i)
         {
             if(s[i] == '(' || s[i] == '[' || s[i] == '{')
-                op_stack.push(s[i]);
-            else if((s[i] == ')' && (op_stack.empty() || op_stack.top() != '(')) ||
-                    (s[i] == ']' && (op_stack.empty() || op_stack.top() != '[')) ||
-                    (s[i] == '}' && (op_stack.empty() || op_stack.top() != '{')))
+                stackForLeftParentheses.push(s[i]);
+            else if(!checkMatchedParentheses(s[i]))
                     return false;
-            else
-                op_stack.pop();
         }
-        return op_stack.empty();
-}
+        
+        return stackForLeftParentheses.empty();
+    }
+private:
+    stack<char> stackForLeftParentheses;
+
+    bool checkMatchedParentheses(const char rightParentheses)
+    {
+        if(stackForLeftParentheses.empty() || 
+           ((int) stackForLeftParentheses.top() != (int) rightParentheses - 1 && 
+           (int) stackForLeftParentheses.top() != (int) rightParentheses - 2))
+            return false;
+        
+        stackForLeftParentheses.pop();
+        return true;
+    }
+};
