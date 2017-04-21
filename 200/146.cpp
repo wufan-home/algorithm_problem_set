ad328eb79fb037fb4a0a1e2c50a7c435af5e2add
Fan Wu <deepexploring@gmail.com>
2017-04-21 00:29:12 -0700

    LRU Cache

--- 200/146.cpp
+++ 200/146.cpp
@@ -1,137 +1,117 @@
-#include <iostream>
-#include <map>
-#include <utility>
+/*
+	Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
 
-using namespace std;
+	get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+	put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+	Follow up:
+	Could you do both operations in O(1) time complexity?
+
+	Example:
+
+	LRUCache cache = new LRUCache( 2 ); //capacity
+
+	cache.put(1, 1);
+	cache.put(2, 2);
+	cache.get(1);       // returns 1
+	cache.put(3, 3);    // evicts key 2
+	cache.get(2);       // returns -1 (not found)
+	cache.put(4, 4);    // evicts key 1
+	cache.get(1);       // returns -1 (not found)
+	cache.get(3);       // returns 3
+	cache.get(4);       // returns 4
+*/
 
 class LRUCache{
 public:
-	LRUCache(int capacity) {
-		m_capacity = capacity;
-		m_head = NULL;
-		m_tail = NULL;
-	}
+    LRUCache(int capacity) : mCapacity(capacity), head(NULL), tail(NULL) {}
     
-	int get(int key) {
-		cout << "Get: " << key << endl;
-		if(m_info_map.find(key) == m_info_map.end())
-			return -1;
-
-		UpdateFreq(m_info_map[key]);
-		return m_info_map[key]->value;
-	}
-	
-	void set(int key, int value) {
-		cout << "Set: " << key << ", " << value << endl;
-		if(m_info_map.find(key) != m_info_map.end())
-		{
-			m_info_map[key]->value = value;
-			UpdateFreq(m_info_map[key]);
-		}
-		else
-		{
-			if(m_info_map.size() >= m_capacity)
-			{
-				cout << "Add: " << key << ", " << value << m_info_map.size() << ", " << m_capacity << endl;
-				int delete_key = m_tail->key;
-				RemoveNode(m_tail);
-				delete m_info_map[delete_key];
-				m_info_map.erase(delete_key);
-			}
-
-			m_info_map[key] = new node(key, value);
-			if(m_tail)
-			{
-				m_tail->next = m_info_map[key];
-				m_info_map[key]->prev = m_tail;
-				m_tail = m_tail->next;
-			}
-			else
-			{
-				m_head = m_info_map[key];
-				m_tail = m_head;
-			}
-			UpdateFreq(m_info_map[key]);
-		}
-
-		for(node *cur = m_head; cur != NULL; cur = cur->next)
-		{
-			cout << cur->key << " - " << cur->value << ", ";
-			if(cur == m_tail)
-			{
-				if(cur->next)
-					cout << "| " << cur->next->key << " - " << cur->next->value;
-				break;
-			}
-		}
-		cout << endl;
-	}
+    int get(int key) {
+        int value = -1;
+        if(container.find(key) != container.end())
+        {
+            value = container[key].first;
+            updatePriorityList(container[key].second);
+        }
+        
+        return value;
+    }
+    
+    void put(int key, int value) {
+        if(container.find(key) != container.end())
+        {
+            container[key].first = value;
+            updatePriorityList(container[key].second);
+            return;
+        }
+        
+        if(container.size() == mCapacity)
+        {
+            int removeKey = tail->key;
+            if(tail->prev == NULL)
+            {
+                delete tail;
+                head = NULL;
+                tail = NULL;
+            }
+            else
+            {
+                tail = tail->prev;
+                delete tail->next;
+                tail->next = NULL;
+            }
+            
+            container.erase(removeKey);
+        }
+        
+        DoubleListNode *newNode = new DoubleListNode(key);
+        container[key] = make_pair(value, newNode);
+        newNode->next = head;
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            head->prev = newNode;
+            head = newNode;
+        }
+    }
+    
 private:
-	struct node
-	{
-		int key;
-		int value;
+    struct DoubleListNode
+    {
+        int key;
+        DoubleListNode *prev;
+        DoubleListNode *next;
+        
+        DoubleListNode(int key) : key(key), prev(NULL), next(NULL) {}
+    };
+    
+    void updatePriorityList(DoubleListNode *curNode)
+    {
+        if(curNode->prev != NULL)
+        {
+            DoubleListNode *prev = curNode->prev;
+            DoubleListNode *next = curNode->next;
+            curNode->prev = NULL;
+            curNode->next = head;
+            head->prev = curNode;
+            head = curNode;
+            
+            prev->next = next;
+            if(next == NULL)
+                tail = prev;
+            else
+                next->prev = prev;
+        }
+    }
 
-		node *prev;
-		node *next;
-		node(int key, int value) : key(key), value(value), prev(NULL), next(NULL) {}
-	};
-	void UpdateFreq(node *n)
-	{
-		cout << "update: " << n->key << endl;
-		RemoveNode(n);
-		if(n)
-		{
-			n->prev = NULL;
-			n->next = m_head;
-			if(m_head)
-				m_head->prev = n;
-			m_head = n;
-		}
-	}
-
-	void RemoveNode(node *n)
-	{
-		
-		if(!n)
-			return;
-
-		if(!n->prev)
-		{
-			m_head = m_head->next;
-			if(m_head)
-				m_head->prev = NULL;
-			return;
-		}
-
-		cout << "Remove" << endl;
-		n->prev->next = n->next;
-		if(n->next)
-			n->next->prev = n->prev;
-		else
-		{
-			m_tail = n->prev;
-			m_tail->next = NULL;
-		}
-
-		
-	}
-	node *m_head;
-	node *m_tail;
-	map<int, node *> m_info_map;
-	int m_capacity;
+    int mCapacity;
+    
+    DoubleListNode *head;
+    DoubleListNode *tail;
+    
+    unordered_map<int, pair<int, DoubleListNode *>> container;
 };
-
-int main()
-{
-	//2,[set(2,1),set(2,2),get(2),set(1,1),set(4,1),get(2)]
-	LRUCache c(2);
-	c.set(2, 1);
-	c.set(2, 2);
-	cout << c.get(2) << endl;
-	c.set(1, 1);
-	c.set(4, 1);
-	cout << c.get(2) << endl;
-	
-	return 1;
-}
