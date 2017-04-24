8d8666a3cc8143998ca84de860dd2b801cbc3fdc
Fan Wu <deepexploring@gmail.com>
2017-04-23 19:41:08 -0700

    N-Queens

--- 100/051.cpp
+++ 100/051.cpp
@@ -1,94 +1,79 @@
-// 51.cpp : Defines the entry point for the console application.
-//
+/*
+	The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+	
+	Given an integer n, return all distinct solutions to the n-queens puzzle.
 
-#include "stdafx.h"
-#include <string>
-#include <vector>
-#include <iostream>
+	Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
 
-using namespace std;
+	For example,
+	There exist two distinct solutions to the 4-queens puzzle:
 
-bool CheckAvail(const vector<string>& board, pair<int, int> position)
-{
-	int n = board.size();
-	int row = position.first;
-	int col = position.second;
-	for (int i = 0; i < row; ++i)
-	{
-		if (board[i][col] == board[row][col])
-			return false;
-	}
+	[
+	 [".Q..",  // Solution 1
+	  "...Q",
+	  "Q...",
+	  "..Q."],
 
-	for (int i = 1; i <= col; ++i)
-	{
-		if (row - i >= 0 && board[row - i][col - i] == board[row][col])
-			return false;
+	 ["..Q.",  // Solution 2
+	  "Q...",
+	  "...Q",
+	  ".Q.."]
+	]
+	Show Tags
+	Show Similar Problems
+*/
 
-		if (row + i < n && board[row + i][col - i] == board[row][col])
-			return false;
-	}
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        size = n;
+        unavailableLines.resize(n, -1);
+        getTableByLine(0);
+        
+        return tables;
+    }
+    
+private:
+    void getTableByLine(int l)
+    {
+        if(l == size)
+        {
+            vector<string> table;
+            for(int i = 0; i < l; ++i)
+            {
+                string line(l, '.');
+                line[unavailableLines[i]] = 'Q';
+                table.push_back(line);
+            }
+            tables.push_back(table);
+            return;
+        }
+        
+        for(int i = 0; i < size; ++i)
+        {
+            if(find(unavailableLines.begin(), unavailableLines.end(), i) != unavailableLines.end() || 
+                checkLeanLines(l, i))
+                continue;
+                
+            unavailableLines[l] = i;
+            getTableByLine(l + 1);
+            unavailableLines[l] = -1;
+        }
+    }
 
-	for (int i = 1; i <= n - col; ++i)
-	{
-		if (row - i >= 0 && board[row - i][col + i] == board[row][col])
-			return false;
+    bool checkLeanLines(int l, int c)
+    {
+        for(int i = 0; l - i >= 0; ++i)
+        {
+            if((c - i >= 0 && c - i == unavailableLines[l - i]) ||
+                (c + i < size && c + i == unavailableLines[l - i]))
+                return true;	
+        }
+       
+        return false;
+    }
 
-		if (row + i < n && board[row + i][col + i] == board[row][col])
-			return false;
-	}
-
-	return true;
-}
-
-void GetSolutions(vector<string>& board, int row, int n, vector<vector<string> >& results)
-{
-	for (int i = 0; i < n; ++i)
-	{
-		board[row][i] = 'Q';
-		if (row == n - 1)
-		{
-			if (CheckAvail(board, make_pair(row, i)))
-				results.push_back(board);
-		}
-		else
-		{
-			if (CheckAvail(board, make_pair(row, i)))
-				GetSolutions(board, row + 1, n, results);
-		}
-		board[row][i] = '.';
-	}
-}
-
-vector<vector<string>> solveNQueens(int n)
-{
-	vector<vector<string> > results;
-	if (n <= 0)
-		return results;
-
-	if (n == 1)
-	{
-		string result("Q");
-		results.push_back(vector<string>(1, result));
-		return results;
-	}
-
-	vector<string> board(n, string(n, '.'));
-	GetSolutions(board, 0, n, results);
-
-	return results;
-}
-
-int main()
-{
-	vector<vector<string> > r = solveNQueens(8);
-	for (int i = 0; i < r.size(); ++i)
-	{
-		for (int j = 0; j < r[i].size(); ++j)
-			cout << r[i][j] << endl;
-
-		cout << endl;
-	}
-	cout << endl;
-    return 0;
-}
-
+   vector<int> unavailableLines;
+   vector<vector<string>> tables;
+   int size;
+};
