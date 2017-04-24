3b28b7be6b34d3ef0c7529a442abc93011c07dec
Fan Wu <deepexploring@gmail.com>
2017-04-23 19:49:19 -0700

    N-Queens II

--- 100/052.cpp
+++ 100/052.cpp
@@ -1,16 +1,52 @@
-//
+/*
+	Follow up for N-Queens problem.
 
-#include <vector>
+	Now, instead outputting board configurations, return the total number of distinct solutions.
+*/
 
-using namespace std;
+class Solution {
+public:
+    int totalNQueens(int n) {
+        size = n;
+        unavailableLines.resize(n, -1);
+        countNQueens(0);
+        return totalNumber;
+    }
+    
+private:
+    void countNQueens(int l)
+    {
+        if(l == size)
+            ++totalNumber;
+        else
+        {
+            for(int c = 0; c < size; ++c)
+            {
+                if(find(unavailableLines.begin(), unavailableLines.end(), c) != unavailableLines.end() || 
+                    checkLeanLines(l, c))
+                    continue;
+                    
+                unavailableLines[l] = c;
+                countNQueens(l + 1);
+                unavailableLines[l] = -1;
+            }
+        }
+    }
+    
+    bool checkLeanLines(int l, int c)
+    {
+        for(int i = 0; l - i >= 0; ++i)
+        {
+            if((c - i >= 0 && c - i == unavailableLines[l - i]) ||
+                (c + i < size && c + i == unavailableLines[l - i]))
+                return true;
+        }
+       
+        return false;
+    }
 
-int totalNQueens(int n) {
-	if(n == 1)
-		return 1;
+    vector<int> unavailableLines;
 
-	if(n <= 3)
-		return 0;
-
-	vector<char> board(n, vector<char>(n, '.'));
-	
-}
+    int size;
+    int totalNumber;
+};
