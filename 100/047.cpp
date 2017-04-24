f29827c91a7c511ecc00c2817d97b905372384b3
Fan Wu <deepexploring@gmail.com>
2017-04-23 18:17:06 -0700

    Permutations II

--- 100/047.cpp
+++ 100/047.cpp
@@ -1,53 +1,39 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+/*
+	Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
-using namespace std;
+	For example,
+	[1,1,2] have the following unique permutations:
+	[
+	  [1,1,2],
+	  [1,2,1],
+	  [2,1,1]
+	]
+*/
 
-vector<vector<int> > permuteUnique(vector<int>& nums) {
-        vector<vector<vector<int> > >perms_alt(2, vector<vector<int> >());
+class Solution {
+public:
+    void getPermutationUnique(vector<int>& nums, int start, vector<vector<int>>& permutations)
+    {
+        if(start == nums.size() - 1)
+            permutations.push_back(nums);
+        else
+        {
+            for(int i = start; i < nums.size();)
+            {
+                for(int j = i; j > start; --j)
+                    swap(nums[j - 1], nums[j]);
+                getPermutationUnique(nums, start + 1, permutations);
+                for(int j = start; j < i; ++j)
+                    swap(nums[j], nums[j + 1]);
+                for(++i; i < nums.size() && nums[i] == nums[i - 1]; ++i){}
+            }
+        }
+    }
 
-	if(nums.empty())
-		return perms_alt[0];
-
-	perms_alt[0].push_back(vector<int>(1, nums[0]));
-	if(nums.size() == 1)
-		return perms_alt[0];
-
-	sort(nums.begin(), nums.end());
-	
-	int size = nums.size();
-	for(int i = 1; i < size; ++i)
-	{
-		int last = (i - 1) % 2;
-		int current = i % 2;
-		for(int j = perms_alt[last].size() - 1; j >= 0; --j)
-		{
-			vector<int> perm = perms_alt[last][j];
-			perm.push_back(nums[i]);
-			perms_alt[current].push_back(perm);
-			for(int l = perm.size() - 1; l > 0; --l)
-			{
-				swap(perm[l], perm[l - 1]);
-				if(perm[l] != perm[l - 1])
-					perms_alt[current].push_back(perm);
-			}
-		}
-		perms_alt[last].clear();
-	}
-
-	return perms_alt[(nums.size() - 1) % 2];
-}
-
-int main()
-{
-	int a[] = { 3,3,0,0,2,3,2 };
-	vector<int> nums(a, a + sizeof(a) / sizeof(int));
-	vector<vector<int> > r = permuteUnique(nums);
-	for(int i = 0; i < r.size(); ++i)
-	{
-		for(int j = 0; j < r[i].size(); ++j)
-			cout << r[i][j] << ", ";
-		cout << endl;
-	}
-}
+    vector<vector<int> > permuteUnique(vector<int> &nums) {
+        vector<vector<int>> permutations;
+        sort(nums.begin(), nums.end());
+        getPermutationUnique(nums, 0, permutations);
+        return permutations;
+    }
+};
