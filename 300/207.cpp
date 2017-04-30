fa2a111ddd6aea8570070d006a75637c956fa600
Fan Wu <deepexploring@gmail.com>
2017-04-29 21:56:09 -0700

    Course Schedule

--- 300/207.cpp
+++ 300/207.cpp
@@ -1,77 +1,72 @@
-#include <vector>
-#include <set>
-#include <stack>
-#include <iostream>
-#include <unordered_set>
-#include <algorithm>
+/*
+	There are a total of n courses you have to take, labeled from 0 to n - 1.
 
-using namespace std;
+	Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
 
-bool TopoSort(vector<vector<int> >& edges, int start,
-	      unordered_set<int>& visited, vector<int>& result)
-{
-	//Kahn algo.
-	if(edges[start].empty())
-	{
-		result.push_back(start);
-		return true;
-	}
+	Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
 
-	while(!edges[start].empty())
-	{
-		vector<int>::iterator it = edges[start].end() - 1;
-		if(find(result.begin(), result.end(), *it) != result.end())
-			edges[start].pop_back();
-		else
-		{
-			if(visited.find(*it) != visited.end())
-				return false;
+	For example:
 
-			visited.insert(*it);
-			if(!TopoSort(edges, *it, visited, result))
-				return false;
-			visited.erase(*it);
-		}
-	}
+	2, [[1,0]]
+	There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
 
-	return true;
-}
+	2, [[1,0],[0,1]]
+	There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
 
-bool canFinish(int numCourses, vector<pair<int, int> >& prerequisites)
-{
-	if(numCourses == 0 || prerequisites.empty())
-		return true;
+	Note:
+	The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+	You may assume that there are no duplicate edges in the input prerequisites.
+	click to show more hints.
 
-	vector<vector<int> > edges(numCourses, vector<int>());
-	for(int i = 0; i < prerequisites.size(); ++i)
-		edges[prerequisites[i].second].push_back(prerequisites[i].first);
+	Hints:
+	This problem is equivalent to finding if a cycle exists in a directed graph. If a cycle exists, no topological ordering exists and therefore it will be impossible to take all courses.
+	Topological Sort via DFS - A great video tutorial (21 minutes) on Coursera explaining the basic concepts of Topological Sort.
+	Topological sort could also be done via BFS.
+*/
 
-	vector<int> result;
-	unordered_set<int> visited;
-	for(int i = 0; i < numCourses; ++i)
-	{
-		if(!TopoSort(edges, i, visited, result))
-			return false;
-	}
-
-	return true;
-}
-
-
-int main()
-{
-	/*8
-[[1,0],[2,6],[1,7],[5,1],[6,4],[7,0],[0,5],[5,1],[6,4]]*/
-	vector<pair<int, int> > pres;
-	pres.push_back(make_pair(1, 0));
-	pres.push_back(make_pair(2, 6));
-	pres.push_back(make_pair(1, 7));
-	pres.push_back(make_pair(5, 1));
-	pres.push_back(make_pair(6, 4));
-	pres.push_back(make_pair(7, 0));
-	pres.push_back(make_pair(0, 5));
-	pres.push_back(make_pair(5, 1));
-	pres.push_back(make_pair(6, 4));
-	cout << canFinish(8, pres) << endl;
-	return 1;
-}
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<vector<int>> dependency(numCourses, vector<int>());
+        
+        for(int i = 0; i < prerequisites.size(); ++i)
+            dependency[prerequisites[i].first].push_back(prerequisites[i].second);
+        
+        stack<int> stackForCourses;
+        vector<bool> finished(numCourses, false);
+        vector<bool> cached(numCourses, false);
+        for(int i= 0; i < numCourses; ++i)
+        {
+            if(!finished[i])
+            {
+                stackForCourses.push(i);
+                while(!stackForCourses.empty())
+                {
+                    int curCourse = stackForCourses.top();
+                    if(finished[curCourse] || dependency[curCourse].empty() || cached[curCourse])
+                    {
+                        finished[curCourse] = true;
+                        cached[curCourse] = false;
+                        stackForCourses.pop();
+                    }
+                    else
+                    {
+                        cached[curCourse] = true;
+                        for(int j = 0; j < dependency[curCourse].size(); ++j)
+                        {
+                            if(finished[dependency[curCourse][j]])
+                                continue;
+                                
+                            if(cached[dependency[curCourse][j]])
+                                return false;
+                                
+                            stackForCourses.push(dependency[curCourse][j]);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+};
