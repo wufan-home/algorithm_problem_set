a5db05eea363c2d916f969ed9703c0b6c8eb6384
Fan Wu <deepexploring@gmail.com>
2017-05-02 00:15:38 -0700

    Implement strStr()

--- 100/028.cpp
+++ 100/028.cpp
@@ -28,3 +28,65 @@ public:
         return -1;
     }
 }
+
+//======================= KMP =========================
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+            return 0;
+            
+        if(haystack.size() == needle.size())
+            return haystack == needle ? 0 : -1;
+
+        // Compute partial match table.
+        vector<int> partialMatchTable(needle.size(), 0);
+        for(int substringLength = 1; substringLength < needle.size(); ++substringLength)
+        {
+            //cout << "substr = " << needle.substr(0, substringLength) << endl;
+            for(int l = substringLength - 1; l > 0 ; --l)
+            {
+                bool match = true;
+                for(int i = 0, j = substringLength - l; i < l; ++i, ++j)
+                {
+                    if(needle[i] != needle[j])
+                        match = false;
+                }
+                /*cout << "left = " << needle.substr(0, l) << " - " 
+                    << "right = " << needle.substr(substringLength - l, l) 
+                    << " - " << match << endl;*/
+                if(match)
+                {
+                    partialMatchTable[substringLength - 1] = l;
+                    break;
+                }
+            }
+        }
+        
+        /*for(int i = 0; i < partialMatchTable.size(); ++i)
+        {
+            cout << needle.substr(0, i + 1) << ", ";
+            cout << partialMatchTable[i] << endl;
+        }
+        cout << endl;*/
+
+        int bound = (int) haystack.size() - (int) needle.size() + 1;
+        for(int i = 0; i < bound;)
+        {
+            int j = 0;
+            for(; j < needle.size(); ++j)
+            {
+                if(haystack[i + j] != needle[j])
+                    break;
+            }
+            
+            if(j == needle.size())
+                return i;
+            else
+                i += j == 0 ? 1 : (j - partialMatchTable[j - 1]);
+        }
+        
+        return -1;
+    }
+};
