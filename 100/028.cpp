12360b2a80172c05e419d8e87a058f336ca44723
Fan Wu <deepexploring@gmail.com>
2017-05-01 21:10:13 -0700

    Implement strStr()

--- 100/028.cpp
+++ 100/028.cpp
@@ -6,27 +6,25 @@
    2) KMP or other methods.
 */
 
-#include <string>
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.empty())
+            return 0;
 
-using namespace std;
-
-int strStr(string haystack, string needle) {
-	if(needle.size() > haystack.size())
-		return -1;
-
-        if(haystack.empty() || needle.empty())
-		return 0;
-
-	for(int i = 0; i < haystack.size() - needle.size(); ++i)
-	{
-		int j = 0;
-		for(; j < needle.size(); ++j)
-		{
-			if(haystack[i + j] != needle[j])
-				break;
-		}
-		if(j == needle.size())
-			return i;
-	}
-	return -1;
+        int bound = (int) haystack.size() - (int) needle.size() + 1;
+        for(int i = 0; i < bound; ++i)
+        {
+            int j = 0;
+            for(; j < needle.size(); ++j)
+            {
+                if(haystack[i + j] != needle[j])
+                    break;
+            }
+            if(j == needle.size())
+                return i;
+        }
+        
+        return -1;
+    }
 }
