1c9437b58bfe28873f1f29341508483972980c34
Fan Wu <deepexploring@gmail.com>
2017-05-03 02:00:11 -0700

    Minimum Window Substring

--- 100/076.cpp
+++ 100/076.cpp
@@ -15,57 +15,66 @@
 
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        const int ss = s.size();
-        const int ts = t.size();
-        
-        if(ts == 0  || ss < ts)
-            return "";
-            
-        vector<int> frequency(256, 0);
-        vector<int> count(256, 0);
-        for(int i = 0; i < t.size(); ++i)
-            ++frequency[t[i]];
-        
-        int minWindowLength = INT_MAX;
-        int finalStart = 0;
-        for(int start = 0, end = 0; end < s.size(); ++end)
-        {
-            if(frequency[s[end]] == 0)
-                continue;
-              
-            ++count[s[end]];
-            if(!isCountLargerOrEqualToFrequency(count, frequency))
-                continue;
-            
-            for(; isCountLargerOrEqualToFrequency(count, frequency); ++start)
-            {
-                if(frequency[s[start]])
-                    --count[s[start]];
-            }
-            ++count[s[--start]];
-            
-            if(end - start + 1 < minWindowLength)
-            {
-                minWindowLength = end - start + 1;
-                finalStart = start;
-            }
-            
-            --count[s[start++]];
-        }
-        
-        return minWindowLength == INT_MAX ? "" : s.substr(finalStart, minWindowLength);
-    }
-    
+	string minWindow(string s, string t) {
+		const int ss = s.size();
+		const int ts = t.size();
+
+		if (ts == 0 || ss < ts)
+			return "";
+
+		unordered_map<char, int> frequency;
+		unordered_map<char, int> count;
+		for (int i = 0; i < t.size(); ++i)
+		{
+			if (frequency.find(t[i]) == frequency.end())
+			{
+				frequency[t[i]] = 0;
+				count[t[i]] = 0;
+			}
+
+			++frequency[t[i]];
+		}
+
+		int minWindowLength = INT_MAX;
+		int finalStart = 0;
+		for (int start = 0, end = 0; end < s.size(); ++end)
+		{
+			if (frequency.find(s[end]) == frequency.end())
+				continue;
+
+			++count[s[end]];
+			if (!isCountLargerOrEqualToFrequency(count, frequency))
+				continue;
+
+			for (; isCountLargerOrEqualToFrequency(count, frequency); ++start)
+			{
+				if (count.find(s[start]) != count.end())
+					--count[s[start]];
+			}
+			++count[s[--start]];
+
+			if (end - start + 1 < minWindowLength)
+			{
+				minWindowLength = end - start + 1;
+				finalStart = start;
+			}
+
+			--count[s[start++]];
+		}
+
+		return minWindowLength == INT_MAX ? "" : s.substr(finalStart, minWindowLength);
+	}
+
 private:
-    bool isCountLargerOrEqualToFrequency(const vector<int>& count, const vector<int>& frequency)
-    {
-        int size = frequency.size();
-        for(int i = 0; i < size; ++i)
-        {
-            if(count[i] < frequency[i])
-                return false;
-        }
-        return true;
-    }
+	bool isCountLargerOrEqualToFrequency(unordered_map<char, int>& count, unordered_map<char, int>& frequency)
+	{
+		for (unordered_map<char, int>::iterator itFreq = frequency.begin(), itCount = count.begin();
+			itFreq != frequency.end();
+			++itFreq, ++itCount)
+		{
+			if (itCount->second < itFreq->second)
+				return false;
+		}
+		return true;
+	}
 };
