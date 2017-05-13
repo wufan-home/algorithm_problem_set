21aeda53fbcc8e748c8150fc87e05bdbaaef3783
Fan Wu <deepexploring@gmail.com>
2017-05-13 15:55:07 -0700

    Convert Sorted List to Binary Search Tree

--- 200/109.cpp
+++ 200/109.cpp
@@ -2,80 +2,54 @@
   Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
 */
 
-#include <iostream>
-#include <climits>
-#include <queue>
-
-using namespace std;
-
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        int size = 0;
+        for(ListNode *cur = head; cur != NULL; cur = cur->next)
+            ++size;
+            
+        return getSubtreeBySize(head, size);
+    }
+    
+private:
+    TreeNode* getSubtreeBySize(ListNode *head, int size)
+    {
+        if(head == NULL) 
+            return NULL;
+        
+        if(head->next == NULL)
+            return new TreeNode(head->val);
+            
+        int leftSize = size / 2;
+        ListNode *newHead = head;
+        for(int i = 0; i < leftSize - 1; ++i, newHead = newHead->next) {}
+        
+        TreeNode *root = new TreeNode(newHead->next->val);
+        
+        ListNode *right = newHead->next->next;
+        newHead->next->next = NULL;
+        newHead->next = NULL;
+        
+        root->left = getSubtreeBySize(head, leftSize);
+        root->right = getSubtreeBySize(right, size - leftSize - 1);
+        return root;
+    }
 };
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
- 
-
-TreeNode *build(ListNode *head, int count)
-{	
-	int left = count / 2;
-	ListNode *lend = head;
-	for(int i = 0; i < left; ++i, lend = lend->next) {}
-	TreeNode *root = new TreeNode(lend->next->val);
-	if(left)
-		root->left = build(head, left);
-	if(count - left - 1)
-		root->right = build(lend->next, count - left - 1);
-	return root;
-}
-
-TreeNode* sortedListToBST(ListNode* head) {
-	if(head == NULL)
-		return NULL;
-
-	int count = 0;
-	for(ListNode *cur = head; cur != NULL; cur = cur->next, ++count) {}
-
-	ListNode *dummy = new ListNode(INT_MIN);
-	dummy->next = head;
-
-	return build(dummy, count);
-}
-
-int main()
-{
-	ListNode *head = new ListNode(1);
-	ListNode *cur = head;
-	for(int i = 2; i <= 7;++i, cur = cur->next)
-		cur->next = new ListNode(i);
-
-	TreeNode *root = sortedListToBST(head);
-	
-	queue<TreeNode *> node_queue;
-	node_queue.push(root);
-	int level = node_queue.size();
-	while(!node_queue.empty())
-	{
-		TreeNode *cur = node_queue.front();
-		node_queue.pop();
-		--level;
-		cout << cur->val << ", ";
-		if(cur->left)
-			node_queue.push(cur->left);
-		if(cur->right)
-			node_queue.push(cur->right);
-		if(level == 0)
-		{
-			level = node_queue.size();
-			cout << endl;
-		}
-	}
-
-	return 1;
-}
