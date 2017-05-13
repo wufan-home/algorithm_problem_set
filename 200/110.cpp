528ae4504d0ae0617ec513951a10ec578fb872e1
Fan Wu <deepexploring@gmail.com>
2017-05-13 16:09:39 -0700

    Balanced Binary Tree

--- 200/110.cpp
+++ 200/110.cpp
@@ -4,45 +4,39 @@
   a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 */
 
-bool checkBalance(TreeNode *root, int& depth)
-{
-	if(root == NULL)
-	{
-		depth = 0;
-		return true;
-	}
-	else
-	{
-		int left = 0;
-		int right = 0;
-		if(!checkBalance(root->left, left) || !checkBalance(root->right, right))
-			return false;
-
-		depth = max(left , right) + 1;
-		return abs(left - right) <= 1;
-	}
-}
-
-bool isBalanced(TreeNode* root) {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
         int depth = 0;
-	return checkBalance(root, depth);
-}
-
-int checkBalance1(TreeNode *root)
-{
-	if(root == NULL)
-    		return 0;
-    	else
-    	{
-    		int left = 0;
-    		int right = 0;
-    		if((left = checkBalance1(root->left)) == -1 || (right = checkBalance1(root->right)) == -1)
-    			return -1;
+        return checkBalance(root, depth);
+    }
     
-    		return abs(left - right) <= 1 ? max(left, right) + 1 : -1;
-    	}
-}
-
-bool isBalanced1(TreeNode* root) {
-	return checkBalance1(root) != -1;
-}
+private:
+    bool checkBalance(TreeNode *root, int& depth)
+    {
+        if(root == NULL)
+        {
+            depth = 0;
+            return true;
+        }
+        
+        int leftDepth = 0;
+        int rightDepth = 0;
+        if(checkBalance(root->left, leftDepth) == false || 
+            checkBalance(root->right, rightDepth) == false || 
+            abs(leftDepth - rightDepth) > 1)
+            return false;
+            
+        depth = max(leftDepth, rightDepth) + 1;
+        return true;
+    }
+};
