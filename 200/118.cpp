220f6aa7257d8fdb9b2c26a27b5ebe7a5ec1f8f4
Fan Wu <deepexploring@gmail.com>
2017-05-14 16:57:03 -0700

    Pascal's Triangle

--- 200/118.cpp
+++ 200/118.cpp
@@ -14,26 +14,28 @@
 
 */
 
-#include <vector>
-
-using namespace std;
-
-vector<vector<int> > generate(int numRows) {
-	vector<vector<int> > rv;
-        if(numRows < 1)
-		return rv;
-
-	rv.push_back(vector<int>(1, 1));
-	if(numRows > 1)
-		rv.push_back(vector<int>(2, 1));
-
-	for(int i = 2; i < numRows; ++i)
-	{
-		vector<int> new_row(i + 1, 1);
-		for(int j = 1; j < i; ++j)
-			new_row[j] = rv[i - 1][j - 1] + rv[i - 1][j];
-		rv.push_back(new_row);
-	}
-
-	return rv;
-}
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangles;
+        if(numRows >= 1)
+            triangles.push_back(vector<int>(1, 1));
+            
+        if(numRows >= 2)
+            triangles.push_back(vector<int>(2, 1));
+            
+        if(numRows >= 3)
+        {
+            for(int i = 3; i <= numRows; ++i)
+            {
+                triangles.push_back(vector<int>(i, 1));
+                vector<int>& prev = triangles[triangles.size() - 2];
+                vector<int>& last = triangles[triangles.size() - 1];
+                for(int j = 1; j < i - 1; ++j)
+                    last[j] = prev[j - 1] + prev[j];
+            }
+        }
+        
+        return triangles;
+    }
+};
