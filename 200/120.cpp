468af0dd2237f81621c915c21f758efd1a8f2652
Fan Wu <deepexploring@gmail.com>
2017-05-14 17:41:48 -0700

    Triangle

--- 200/120.cpp
+++ 200/120.cpp
@@ -1,51 +1,35 @@
 /*
-  Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+	Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
 
-  For example, given the following triangle
+	For example, given the following triangle
 
-[
-     [2],
-    [3,4],
-   [6,5,7],
-  [4,1,8,3]
-]
+	[
+		 [2],
+		[3,4],
+	   [6,5,7],
+	  [4,1,8,3]
+	]
 
-The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+	The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
 
-Note:
-Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle. 
+	Note:
+	Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle. 
 */
 
-#include <iostream>
-#include <vector>
-#include <climits>
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if(triangle.empty())
+            return INT_MIN;
 
-using namespace std;
-
-int minimumTotal(vector<vector<int> >& triangle) {
-	if(triangle.size() < 1)
-		return INT_MIN;
-	else if(triangle.size() == 1)
-		return triangle[0][0];
-	else
-	{
-		int row = triangle.size() - 1;
-		vector<int> psums(triangle[row]);
-		for(--row; row >= 0; --row)
-		{
-			for(int i = 0; i < triangle[row].size(); ++i)
-				psums[i] = triangle[row][i] + min(psums[i], psums[i + 1]);
-		}
-		return psums[0];
-	}
-}
-
-int main()
-{
-	vector<vector<int> > ts(1, vector<int>({2}));
-	ts.push_back(vector<int>({3, 4}));
-	//ts.push_back(vector<int>({6, 5, 7}));
-	//ts.push_back(vector<int>({4, 1, 8, 3}));
-	cout << minimumTotal(ts) << endl;
-	return 1;
-}
+        vector<int> levelSums(triangle[triangle.size() - 1]);
+        for(int i = triangle.size() - 2; i >= 0; --i)
+        {
+            const vector<int>& level = triangle[i];
+            for(int j = 0; j < level.size(); ++j)
+                levelSums[j] = level[j] + min(levelSums[j], levelSums[j + 1]);
+        }
+        
+        return levelSums[0];
+    }
+};
