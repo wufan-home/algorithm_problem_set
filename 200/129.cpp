184615e990b5a6d463fb0c30a244c9999b22808e
Fan Wu <deepexploring@gmail.com>
2017-05-14 19:46:32 -0700

    Sum Root to Leaf Numbers

--- 200/129.cpp
+++ 200/129.cpp
@@ -1,54 +1,57 @@
 /*
-  Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
-  An example is the root-to-leaf path 1->2->3 which represents the number 123.
-  Find the total sum of all root-to-leaf numbers.
-  For example,
+	Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+	An example is the root-to-leaf path 1->2->3 which represents the number 123.
+	Find the total sum of all root-to-leaf numbers.
+	For example,
 
-    1
-   / \
-  2   3
-  The root-to-leaf path 1->2 represents the number 12.
-  The root-to-leaf path 1->3 represents the number 13.
-  Return the sum = 12 + 13 = 25.
+		1
+	   / \
+	  2   3
+	The root-to-leaf path 1->2 represents the number 12.
+	The root-to-leaf path 1->3 represents the number 13.
+	Return the sum = 12 + 13 = 25.
 */
 
-int sumNumbers(TreeNode* root) {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    Solution() : current(0), sum(0) {}
+    
+    int sumNumbers(TreeNode* root) {
         if(root == NULL)
-		return 0;
+            return 0;
+            
+        calculateSum(root);
+        return sum;
+    }
+private:
+    void calculateSum(TreeNode *root)
+    {
+        current = 10 * current + root->val;
+        if(root->left == NULL && root->right == NULL)
+        {
+            sum += current;
+            current /= 10;
+            return;
+        }
+        
+        if(root->left)
+            calculateSum(root->left);
+            
+        if(root->right)
+            calculateSum(root->right);
+            
+        current /= 10;
+    }
 
-	stack<TreeNode *> node_stack;
-	TreeNode *cur = root;
-	bool visited = false;
-	long long num = 0;
-	int sum = 0;
-	while(cur || !node_stack.empty())
-	{
-		if(cur)
-		{
-			num *= 10;
-			num += cur->val;
-			node_stack.push(cur);
-			cur = cur->left;
-		}
-		else
-		{
-			cur = node_stack.top();
-
-			if(cur->right && !visited)
-				cur = cur->right;
-			else
-			{
-				if(cur->left == NULL && cur->right == NULL)
-					sum += num;
-
-				num /= 10;
-				node_stack.pop();
-				visited = (cur == node_stack.top()->right);
-				cur = NULL;
-			}
-		}
-	}
-
-	return sum;
-}
-
+    int current;
+    int sum;
+};
