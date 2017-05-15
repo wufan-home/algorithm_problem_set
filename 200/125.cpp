3fd000e4bfe0323f9e98cfbd6e9c2822dc3a7b9c
Fan Wu <deepexploring@gmail.com>
2017-05-14 18:35:42 -0700

    Valid Palindrome

--- 200/125.cpp
+++ 200/125.cpp
@@ -5,29 +5,27 @@
   "race a car" is not a palindrome.
 */
 
-bool isPalindrome(string s) {
-        if(s.empty())
-    		return true;
-    
-    	for(int l = 0, r = s.size() - 1; l <= r;)
-    	{
-    		if((s[l] < 'A' || s[l] > 'Z') && (s[l] < 'a' || s[l] > 'z') && (s[l] < '0' || s[l] > '9'))
-    			++l;
-    		else if((s[r] < 'A' || s[r] > 'Z') && (s[r] < 'a' || s[r] > 'z') && (s[r] < '0' || s[r] > '9'))
-    			--r;
-    		else
-    		{
-    			if(s[l] >= 'A' && s[l] <= 'Z')
-    				s[l] = s[l] - 'A' + 'a';
-    
-    			if(s[r] >= 'A' && s[r] <= 'Z')
-    				s[r] = s[r] - 'A' + 'a';
-    
-    			if(s[l++] != s[r--])
-    				return false;
-    		}
-    			
-    	}
-    
-    	return true;
-}
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for(int l = 0, r = s.size() - 1; l < r;)
+        {
+            if(!((s[l] >= 'a' && s[l] <= 'z') || (s[l] >= 'A' && s[l] <= 'Z') || (s[l] >= '0' && s[l] <= '9')))
+                ++l;
+            else if(!((s[r] >= 'a' && s[r] <= 'z') || (s[r] >= 'A' && s[r] <= 'Z') || (s[r] >= '0' && s[r] <= '9')))
+                --r;
+            else 
+            {
+                int lvalue = s[l] - (int)(s[l] >= 'a' && s[l] <= 'z' ? 'a' : 'A');
+                int rvalue = s[r] - (int)(s[r] >= 'a' && s[r] <= 'z' ? 'a' : 'A');
+                if(lvalue != rvalue)
+                    return false;
+                    
+                ++l;
+                --r;
+            }
+        }
+        
+        return true;
+    }
+};
