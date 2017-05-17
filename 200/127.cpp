3a954ef2f535c7d67fbc5bf87c9ba560d8d60ea4
Fan Wu <deepexploring@gmail.com>
2017-05-16 22:22:54 -0700

    Word Ladder

--- 200/127.cpp
+++ 200/127.cpp
@@ -12,142 +12,100 @@
   return its length 5.
 */
 
-#include <string>
-#include <queue>
-#include <iostream>
-#include <unordered_set>
-#include <unordered_map>
-
-using namespace std;
-
-int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList)
-{
-        wordList.erase(beginWord);
-        wordList.erase(endWord);
-        if(wordList.empty())
-		return 0;
-            
-        int len = beginWord.size();
-
-        queue<string> left_q;
-        left_q.push(beginWord);
-        unordered_map<string, int> left_dis;
-        left_dis[beginWord] = 0;
-
-	queue<string> right_q;
-        left_q.push(endWord);
-        unordered_map<string, int> right_dis;
-        right_dis[endWord] = 0;
-
-	while(!left_q.empty() && !right_q.empty())
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict;
+        for(int i = 0; i < wordList.size(); ++i)
         {
-		string l = left_q.front();
-		left_q.pop();
-		for(int i = 0; i < len; ++i)
-		{
-			string s = l;
-			for(char ch = 'a'; ch <= 'z'; ++ch)
-			{
-				l[i] = ch;
-				if(wordList.find(l) != wordList.end() && left_dis.find(l) == left_dis.end())
-				{
-					if(right_dis.find(l) != right_dis.end())
-						return left_dis[s] + right_dis[l];
-					
-					left_dis[l] = left_dis[s] + 1;
-					left_q.push(l);
-				}
-			}
-			l = s;
-		}
-		
-		string r = right_q.front();
-		right_q.pop();
-		for(int i = 0; i < len; ++i)
-		{
-			string s = r;
-			for(char ch = 'a'; ch <= 'z'; ++ch)
-			{
-				r[i] = ch;
-				if(wordList.find(r) != wordList.end() && right_dis.find(r) == right_dis.end())
-					right_dis[r] = right_dis[s] + 1;
-			}
-			r = s;
-		}
+            if(wordList[i] == beginWord || wordList[i] == endWord)
+                continue;
+                
+            dict.insert(wordList[i]);
         }
-	
+        
+        beginToWord[beginWord] = 1;
+        queueFromBegin.push(beginWord);
+        
+        wordToEnd[endWord] = 1;
+        queueFromEnd.push(endWord);
+
+        while(!queueFromBegin.empty() && !queueFromEnd.empty())
+        {
+            string word = queueFromBegin.front();
+            queueFromBegin.pop();
+            int length = searchAdjacentWords(word, dict, true);
+            if(length > 0)
+                return length;
+                
+            word = queueFromEnd.front();
+            queueFromEnd.pop();
+            length = searchAdjacentWords(word, dict, false);
+            if(length > 0)
+                return length;
+        }
+        
         return 0;
-}
+    }
+private:
+    int searchAdjacentWords(const string& word, const unordered_set<string>& dict, bool forward)
+    {
+        //cout << "Now " << word << " ========== " << (forward ? "From start" : "From End") << endl;
+        string cur = word;
+        int len = cur.size();
+        for(int i = 0; i < len; ++i)
+        {
+            for(char c = 'a'; c <= 'z'; ++c)
+            {
+                cur[i] = c;
+                if(dict.find(cur) == dict.end())
+                    continue;
 
-int main()
-{
-	string start = "a";
-	string end = "c";
-	unordered_set<string> dict({"a", "b", "c"});
-	cout << ladderLength(start, end, dict) << endl;
-	return 1;
-}
+                if(forward)
+                {
+                    if(wordToEnd.find(cur) != wordToEnd.end())
+                    {
+                        /*cout << "forward " << word << " -> " << cur << endl;
+                        cout << beginToWord[word] << endl;
+                        cout << wordToEnd[cur] << endl;*/
+                        return beginToWord[word] + wordToEnd[cur];
+                    }
+                    
+                    if(beginToWord.find(cur) != beginToWord.end())
+                        continue;
 
-/*
-int GetMinimalPath(const string& start, const string& end, 
-                       unordered_map<string, vector<string> >& adj,
-                       unordered_map<string, int>& dis)
-{
-	vector<string>& nbs = adj[start];
-	for(int i = 0; i < nbs.size(); ++i)
-	{
-		if(nbs[i] == end)
-		{
-			dis[start] = 2;
-			break;
-		}
-		else
-		{
-			int len = dis[nbs[i]] != INT_MAX ? dis[nbs[i]] : GetMinimalPath(nbs[i], end, adj, dis);
-			dis[start] = min(dis[start], len == INT_MAX ? len : 1 + len);
-		}
-	}
-	return dis[start];
-}
+                    beginToWord[cur] = beginToWord[word] + 1;
+                    queueFromBegin.push(cur);
+                    //cout << cur << ": " << beginToWord[cur] << endl;
+                }
+                else
+                {
+                    if(beginToWord.find(cur) != beginToWord.end())
+                    {
+                        /*cout << "backword " << cur << " -> " << word << endl;
+                        cout << wordToEnd[word] << endl;
+                        cout << beginToWord[cur] << endl;*/
+                        return beginToWord[cur] + wordToEnd[word];
+                    }
+                    
+                    if(wordToEnd.find(cur) != wordToEnd.end())
+                        continue;
+
+                    wordToEnd[cur] = wordToEnd[word] + 1;
+                    queueFromEnd.push(cur);
+                    //cout << cur << ": " << wordToEnd[cur] << ", " << "backward" << endl;
+                }
+            }
+            
+            cur = word;
+        }
         
-int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList)
-{
-	wordList.insert(endWord);
-	wordList.erase(beginWord);
-	if(wordList.empty())
-    		return 0;
-	
-	int len = beginWord.size();
-	unordered_map<string, vector<string> > adj;
-	unordered_map<string, int> dis;
-        
-	queue<string> word_queue;
-	word_queue.push(beginWord);
-	while(!word_queue.empty())
-	{
-		string cur = word_queue.front();
-		word_queue.pop();
-		adj[cur] = vector<string>();
-		dis[cur] = INT_MAX;
-            	
-		for(int i = 0; i < len; ++i)
-		{
-			string s = cur;
-			for(char ch = 'a'; ch <= 'z'; ++ch)
-			{
-				cur[i] = ch;
-				if(wordList.find(cur) != wordList.end())
-				{
-					adj[s].push_back(cur);
-					word_queue.push(cur);
-					wordList.erase(cur);
-				}
-			}
-			cur = s;
-		}
-	}
-        
-	int min_len = GetMinimalPath(beginWord, endWord, adj, dis);
-	return min_len == INT_MAX ? 0 : min_len;
-}
-*/
+        return 0;
+    }
+
+    unordered_map<string, int> beginToWord;
+    queue<string> queueFromBegin;
+    
+    unordered_map<string, int> wordToEnd;
+    queue<string> queueFromEnd;
+};
