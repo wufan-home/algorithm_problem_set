5bb1181ac385a1926f5421be60f4bee7a7d6931d
Fan Wu <deepexploring@gmail.com>
2017-05-21 17:41:41 -0700

    Surrounded Regions

--- 200/130.cpp
+++ 200/130.cpp
@@ -13,68 +13,76 @@
   X O X X
 */
 
-#include <iostream>
-#include <queue>
-#include <utility>
-#include <vector>
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        if(board.empty() || board[0].empty())
+            return;
+            
+        for(int i = 0; i < board.size(); ++i)
+        {
+            if(board[i][0] == 'O')
+                exploreRegion(board, i, 0);
+                
+            if(board[i][board[0].size() - 1] == 'O')
+                exploreRegion(board, i, board[0].size() - 1);
+        }
+        
+        for(int i = 0; i < board[0].size(); ++i)
+        {
+            if(board[0][i] == 'O')
+                exploreRegion(board, 0, i);
+                
+            if(board[board.size() - 1][i] == 'O')
+                exploreRegion(board, board.size() - 1, i);
+        }
 
-using namespace std;
-
-void RepalceAux(vector<vector<char> >& board, int x, int y, char rpl)
-{
-	if(board[x][y] != 'O')
-		return;
-
-	queue<pair<int, int> > node_queue;
-	board[x][y] = rpl;
-	node_queue.push(make_pair(x, y));
-	while(!node_queue.empty())
-	{
-		pair<int, int> cur = node_queue.front();
-		node_queue.pop();
-		for(int i = -1; i <= 1; i += 2)
-		{
-			if(cur.first + i >= 0 && cur.first + i < board.size() &&
-			   board[cur.first + i][cur.second] == 'O')
-			{
-				board[cur.first + i][cur.second] = rpl;
-				node_queue.push(make_pair(cur.first + i, cur.second));
-			}
-
-			if(cur.second + i >= 0 && cur.second + i < board[0].size() &&
-			   board[cur.first][cur.second + i] == 'O')
-			{
-				board[cur.first][cur.second + i] = rpl;
-				node_queue.push(make_pair(cur.first, cur.second + i));
-			}
-		}
-	}
-}
-
-void solve(vector<vector<char> >& board) {
-	if(board.size() <= 2 || board[0].size() <= 2)
-		return;
-
-	for(int j = 0; j < board[0].size(); ++j)
-	{
-		RepalceAux(board, 0, j, 'N');
-		RepalceAux(board, board.size() - 1, j, 'N');
-	}
-
-	for(int i = 1; i < board.size() - 1; ++i)
-	{
-		RepalceAux(board, i, 0, 'N');
-		RepalceAux(board, i, board[0].size() - 1, 'N');
-	}
-	
-	for(int i = 0; i < board.size(); ++i)
-	{
-		for(int j = 0; j < board[0].size(); ++j)
-		{
-			if(board[i][j] == 'N')
-				board[i][j] = 'O';
-			else if(board[i][j] == 'O')
-				board[i][j] = 'X';
-		}
-	}
-}
+        for(int i = 1; i < board.size() - 1; ++i)
+        {
+            for(int j = 1; j < board[0].size() - 1; ++j)
+            {
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+            }
+        }
+        
+        for(int i = 0; i < board.size(); ++i)
+        {
+            for(int j = 0; j < board[0].size(); ++j)
+            {
+                if(board[i][j] == 'Y')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+private:
+    void exploreRegion(vector<vector<char>>& board, int r, int c)
+    {
+        board[r][c] = 'Y';
+        queue<pair<int, int>> queueForCoordinates;
+        queueForCoordinates.push(make_pair(r, c));
+        while(!queueForCoordinates.empty())
+        {
+            int x = queueForCoordinates.front().first;
+            int y = queueForCoordinates.front().second;
+            queueForCoordinates.pop();
+            
+            for(int i = 0; i < 4; ++i)
+            {
+                if(x + step[i][0] < 0 || x + step[i][0] >= board.size())
+                    continue;
+                    
+                if(y + step[i][1] < 0 || y + step[i][1] >= board[0].size())
+                    continue;
+                    
+                if(board[x + step[i][0]][y + step[i][1]] == 'O')
+                {
+                    board[x + step[i][0]][y + step[i][1]] = 'Y';
+                    queueForCoordinates.push(make_pair(x + step[i][0], y + step[i][1]));
+                }
+            }
+        }
+    }
+    
+    int step[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+};
