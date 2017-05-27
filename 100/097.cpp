4d705419d9eaa50721110f5fef03997b7d4a1d07
Fan Wu <deepexploring@gmail.com>
2017-05-27 00:53:35 -0700

    Interleaving String

--- 100/097.cpp
+++ 100/097.cpp
@@ -10,84 +10,37 @@ When s3 = "aadbbcbcac", return true.
 When s3 = "aadbbbaccc", return false. 
 */
 
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-bool isInterleaving(const string& s1, const string& s2, const string& s3)
-{
-	if(s1.size() + s2.size() != s3.size())
-		return false;
-
-	if(s1.empty())
-		return s2 == s3;
-	else if(s2.empty())
-		return s1 == s3;
-
-	int row = s1.size();
-	int col = s2.size();
-	vector<vector<int> > dp(row + 1, vector<int>(col + 1, -1));
-
-	dp[0][0] = 0;
-
-	for(int i = 0; i < s1.size(); ++i)
-	{
-		if(s1[i] != s3[i])
-		{
-			for(int j = i + 1; j <= row; ++j)
-				dp[j][0] = 0;
-			break;
-		}
-		dp[i + 1][0] = 1;
-	}
-
-	for(int i = 0; i < s2.size(); ++i)
-	{
-		if(s2[i] != s3[i])
-		{
-			for(int j = i + 1; j <= col; ++j)
-				dp[0][j] = 0;
-			break;
-		}
-		dp[0][i + 1] = 1;
-	}
-
-	for(int i = 0; i < row; ++i)
-	{
-		for(int j = 0; j < col; ++j)
-		{
-			if(dp[i + 1][j + 1] != -1)
-				continue;
-			cout << i << ": " << s1[i] << ", " << j << ": " << s2[j] << ", " << i + j + 1 << ": " << s3[i + j + 1] << endl;
-			if(s1[i] == s3[i + j + 1] && s2[j] == s3[i + j + 1])
-				dp[i + 1][j + 1] = (dp[i][j + 1] || dp[i + 1][j]);
-			else if(s1[i] == s3[i + j + 1])
-				dp[i + 1][j + 1] = dp[i][j + 1];
-			else if(s2[j] == s3[i + j + 1])
-				dp[i + 1][j + 1] = dp[i + 1][j];
-			else
-				dp[i + 1][j + 1] = 0;
-		}
-	}
-
-	cout << endl;
-	for(int i = 0; i <= row; ++i)
-	{
-		for(int j = 0; j <= col; ++j)
-			cout << dp[i][j] << ", ";
-		cout << endl;
-	}
-	
-	return dp[row][col];
-}
-
-int main()
-{
-	string s1 = "ef";
-	string s2 = "gh";
-	string s3 = "ehgf";
-	cout << isInterleaving(s1, s2, s3) << endl;
-	return 1;
-}
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if(s1.size() + s2.size() != s3.size())
+            return false;
+            
+        if(s1.empty())
+            return s2 == s3;
+            
+        if(s2.empty())
+            return s1 == s3;
+            
+        int lenS1 = s1.size();
+        int lenS2 = s2.size();
+        vector<vector<bool>> interleaveMatrix(lenS1 + 1, vector<bool>(lenS2 + 1, false));
+        for(int r = 0; r <= lenS1; ++r)
+        {
+            for(int c = 0; c <= lenS2; ++c)
+            {
+                if(r == 0 && c == 0)
+                    interleaveMatrix[0][0] = true;
+                else if(r == 0)
+                    interleaveMatrix[r][c] = (s2[c - 1] == s3[c - 1] && interleaveMatrix[r][c - 1]);
+                else if(c == 0)
+                    interleaveMatrix[r][c] = (s1[r - 1] == s3[r - 1] && interleaveMatrix[r - 1][c]);
+                else
+                    interleaveMatrix[r][c] = (s1[r - 1] == s3[r + c - 1] && interleaveMatrix[r - 1][c]) || 
+                                             (s2[c - 1] == s3[r + c - 1] && interleaveMatrix[r][c - 1]);
+            }
+        }
+        
+        return interleaveMatrix[lenS1][lenS2];
+    }
+};
