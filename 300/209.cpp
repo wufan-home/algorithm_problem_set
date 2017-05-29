d4353268bbd919d636e20f62ea219580bd95017e
Fan Wu <deepexploring@gmail.com>
2017-05-29 16:46:34 -0700

    Minimum Size Subarray Sum

--- 300/209.cpp
+++ 300/209.cpp
@@ -1,32 +1,36 @@
-#include <iostream>
-#include <vector>
+/*
+	Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
 
-using namespace std;
+	For example, given the array [2,3,1,2,4,3] and s = 7,
+	the subarray [4,3] has the minimal length under the problem constraint.
 
-int minSubArrayLen(int s, vector<int>& nums) {
+	click to show more practice.
+
+	More practice:
+	If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+*/
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
         if(nums.empty())
-		return 0;
-
-	int min_len = nums.size() + 1;
-	int sum = 0;
-	for(int tail = 0, head = 0; head < nums.size(); ++head)
-	{
-		sum += nums[head];
-		if(sum >= s)
-		{
-			for(; tail <= head && sum >= s; ++tail)
-			{
-				if(head - tail + 1 < min_len)
-					min_len = head - tail + 1;
-				sum -= nums[tail];
-			}
-		}
-	}
-
-	return min_len > nums.size() ? 0 : min_len;
-}
-
-int main()
-{
-	return 1;
-}
+            return 0;
+            
+        int minLen = INT_MAX;
+        int sum = 0;
+        for(int l = 0, r = 0; r < nums.size(); ++r)
+        {
+            sum += nums[r];
+            if(sum < s)
+                continue;
+            
+            for(; l <= s && sum >= s; ++l)
+            {
+                minLen = min(minLen, r - l + 1);
+                sum -= nums[l];
+            }
+        }
+        
+        return minLen == INT_MAX ? 0 : minLen;
+    }
+};
