2229b63003260ef81ff9f476b3d2c24645132f35
Fan Wu <deepexploring@gmail.com>
2017-07-07 04:27:32 -0700

    Binary Tree Maximum Path Sum

--- 200/124.cpp
+++ 200/124.cpp
@@ -1,27 +1,55 @@
+/*
+	Given a binary tree, find the maximum path sum.
 
-#include <vector>
+	For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree 
+	along the parent-child connections. The path must contain at least one node and does not need to go through the root.
 
-using namespace std;
+	For example:
+	Given the below binary tree,
 
-int maxProfit(int k, vector<int>& prices) {
-        if(prices.empty() || k == 0)
-		return 0;
+		   1
+		  / \
+		 2   3
+	Return 6.
+*/
 
-	int size = prices.size();
-	vector<vector<int> > dp(k, vector<int>(prices.size(), 0));
-	for(int i = 0; i < k; ++i)
-	{
-		if(i == 0)
-		{
-			int min_price;
-			for(int j = 0; j < size; ++i)
-			{
-				if(prices[j] )
-			}
-		}
-		else
-		{}
-	}
-
-	return dp[k - 1][prices.size() - 1];
-}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int pathSumOverRoot = INT_MIN;
+        int pathSumFromRoot = getMaxPathSum(root, pathSumOverRoot);
+        return max(pathSumFromRoot, pathSumOverRoot);
+    }
+    
+private:
+    int getMaxPathSum(TreeNode *root, int& maxPathSumOverRoot)
+    {
+        if(root == NULL)
+            return 0;
+        
+        if(root->left == NULL && root->right == NULL)
+        {
+            maxPathSumOverRoot = max(maxPathSumOverRoot, root->val);
+            return root->val;
+        }
+        
+        int localMaxPathSumOverRoot = INT_MIN;
+        int leftMaxPathSum = getMaxPathSum(root->left, localMaxPathSumOverRoot);
+        int rightMaxPathSum = getMaxPathSum(root->right, localMaxPathSumOverRoot);
+        
+        int pathSumOverRoot = root->val + max(0, leftMaxPathSum) + max(0, rightMaxPathSum);
+        
+        maxPathSumOverRoot = max(maxPathSumOverRoot, max(localMaxPathSumOverRoot, pathSumOverRoot));
+        
+        return root->val + max(0, max(leftMaxPathSum, rightMaxPathSum));
+    }
+};
