f4a920b9a0d88ff5f15c167ee51d69396abb07b4
Fan Wu <deepexploring@gmail.com>
2017-08-06 11:59:27 -0700

    Number of Islands II

---
+++ 400/305.cpp
@@ -0,0 +1,93 @@
+/*
+  A 2d grid map of m rows and n columns is initially filled with water. 
+  We may perform an addLand operation which turns the water at position (row, col) into a land. 
+  Given a list of positions to operate, count the number of islands after each addLand operation. 
+  An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
+  You may assume all four edges of the grid are all surrounded by water.
+
+  Example:
+
+  Given m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]].
+  Initially, the 2d grid grid is filled with water. (Assume 0 represents water and 1 represents land).
+
+  0 0 0
+  0 0 0
+  0 0 0
+  Operation #1: addLand(0, 0) turns the water at grid[0][0] into a land.
+
+  1 0 0
+  0 0 0   Number of islands = 1
+  0 0 0
+  Operation #2: addLand(0, 1) turns the water at grid[0][1] into a land.
+
+  1 1 0
+  0 0 0   Number of islands = 1
+  0 0 0
+  Operation #3: addLand(1, 2) turns the water at grid[1][2] into a land.
+
+  1 1 0
+  0 0 1   Number of islands = 2
+  0 0 0
+  Operation #4: addLand(2, 1) turns the water at grid[2][1] into a land.
+
+  1 1 0
+  0 0 1   Number of islands = 3
+  0 1 0
+  We return the result as an array: [1, 1, 2, 3]
+
+  Challenge:
+
+  Can you do it in time complexity O(k log mn), where k is the length of the positions?
+*/
+
+class Solution {
+public:
+    int steps[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
+        int size = positions.size();
+        vector<int> numberOfIslandsList(size, 0);
+        
+        vector<int> board = vector<int>(m * n, -1);
+        unordered_set<int> islandLabel;
+        for(int i = 0; i < size; ++i)
+        {
+            int x = positions[i].first;
+            int y = positions[i].second;
+            int pos = x * n + y;
+            
+            board[pos] = pos;
+            islandLabel.insert(pos);
+            
+            for(int j = 0; j < 4; ++j)
+            {
+                int adjX = x + steps[j][0];
+                int adjY = y + steps[j][1];
+                
+                int adjPos = adjX * n + adjY;
+                
+                if(adjX < 0 || adjX >= m || adjY < 0 || adjY >= n || board[adjPos] < 0)
+                    continue;
+                
+                while(adjPos != board[adjPos])
+                    adjPos = board[adjPos];
+                
+                if(adjPos < pos)
+                {
+                    board[pos] = adjPos;
+                    islandLabel.erase(pos);
+                    pos = adjPos;
+                }
+                else if(adjPos > pos)
+                {
+                    board[adjPos] = pos;
+                    islandLabel.erase(adjPos);
+                }
+            }
+            
+            numberOfIslandsList[i] = islandLabel.size();
+        }
+            
+        return numberOfIslandsList;
+    }
+};
