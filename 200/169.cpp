d44e4f9834a885b76fb36ab18aadda763484f544
Fan Wu <deepexploring@gmail.com>
2017-08-27 23:39:48 -0700

    Majority Element

--- 200/169.cpp
+++ 200/169.cpp
@@ -4,7 +4,10 @@
 
     You may assume that the array is non-empty and the majority element always exist in the array.
     
-    Solution: The vote algorithm.
+    Solution: The vote algorithm. The nature of this algorithm is that the addition is order not matter.
+    This means that it is equivelant that to increase the freq before or afterhand.
+    The majority element (the maximal freq) will always be left, since its freq is bigger than 
+    the sum of the freqs of all other elements.
 */
 
 class Solution {
