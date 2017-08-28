57eadaae9865d25cccd52a3982d50800bf13e911
Fan Wu <deepexploring@gmail.com>
2017-08-27 23:35:58 -0700

    Two Sum III - Data structure design

---
+++ 200/170.cpp
@@ -0,0 +1,52 @@
+/*
+    Design and implement a TwoSum class. It should support the following operations: add and find.
+
+    add - Add the number to an internal data structure.
+    find - Find if there exists any pair of numbers which sum is equal to the value.
+
+    For example,
+    add(1); add(3); add(5);
+    find(4) -> true
+    find(7) -> false
+*/
+
+class TwoSum {
+public:
+    /** Initialize your data structure here. */
+    TwoSum() {
+        
+    }
+    
+    /** Add the number to an internal data structure.. */
+    void add(int number) {
+        ++mNums[number];
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    bool find(int value) {
+        unordered_set<int> m;
+        for(auto a : mNums)
+        {
+            if(a.second > 1 && value == a.first + a.first)
+                return true;
+                
+            int diff = value - a.first;
+            if(m.find(diff) != m.end())
+                return true;
+            else
+                m.insert(a.first);
+        }
+        
+        return false;
+    }
+    
+private:
+    unordered_map<int, int> mNums;
+};
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * bool param_2 = obj.find(value);
+ */
