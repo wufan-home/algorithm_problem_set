2e475e16529455df59e924e6acc12bdd3962d1ea
Fan Wu <deepexploring@gmail.com>
2017-09-03 10:17:29 -0700

    Maximum XOR of Two Numbers in an Array

--- 500/421.cpp
+++ 500/421.cpp
@@ -12,6 +12,21 @@
     Output: 28
 
     Explanation: The maximum result is 5 ^ 25 = 28.
+    
+    Solution:
+    1. The maximal result must be (in decimal form) combination of 0 and 1,
+       and 1 must be placed as left as best.
+    2. The key idea of this solution is to use a prefix tree (trie) method to test all elements of input.
+    3. The candidate prefix will increase a digit 1 each round. 
+       And then, use this prefix to do the XOR to every prefix in the set. 
+       If there are two prefixs combine this prefix 
+       (Here, we use a typical application of hash sets, i.e., check the result exists in the S)
+       generate this prefix, the newly added 1 will be remained. Otherwise, the 1 will be discarded,
+       and then the new digit will be 0 autometically.
+    4. We do not need to worry about we use different pairs of intergers in step 3. 
+       This search is done by prefix. 
+       Hence, even we use different pair of integers, they also belong to the same branch of the prefix tree.
+       This is the reason why we say the key idea is to apply the prefix tree method.
 */
 
 class Solution {
