88d1dcefe6bc40340a13c41c44f1ff41f57e5210
Fan Wu <deepexploring@gmail.com>
2017-10-15 14:23:11 -0700

    Longest Palindromic Substring

--- 100/005.cpp
+++ 100/005.cpp
@@ -2,27 +2,38 @@
 	Given a string S, find the longest palindromic substring in S. 
 	You may assume that the maximum length of S is 1000, 
 	and there exists one unique longest palindromic substring.
+	
+	1. DP method: This is a typical to solve the questions about palingdromes.
+	2. Manacher's Algorithm.
 */
 
-string longestPalindrome(string s) {
-        if(s.size() <= 1)
-            return s;
-            
+// DP
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if(s.empty())
+            return "";
+        
+        int size = s.size();
+        bool dp[size][size] = {false};
         int start = 0;
-        int max_len = 1;
-        for(int i = 0; i < s.size() - max_len / 2; )
+        int maxLen = 0;
+        for(int len = 1; len <= size; ++len)
         {
-            int l = i;
-            int r = i;
-            for(; r < s.size() - 1 && s[r + 1] == s[r]; ++r) {}
-            i = r + 1;
-            for(; l > 0 && r < s.size() - 1 && s[l - 1] == s[r + 1]; --l, ++r) {}
-            if(r - l + 1 > max_len)
+            for(int i = 0; i < size - len + 1; ++i)
             {
-                max_len = r - l + 1;
-                start = l;
+                if(s[i] == s[i + len - 1] && (len <= 2 || dp[i + 1][i + len - 2]))
+                {
+                    dp[i][i + len - 1] = true;
+                    if(len > maxLen)
+                    {
+                        start = i;
+                        maxLen = len;
+                    }
+                }
             }
         }
-
-        return s.substr(start, max_len);
-}
+        
+        return s.substr(start, maxLen);
+    }
+};
