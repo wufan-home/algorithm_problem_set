4219aa8c9f46a898d540d83ae0a89fb6601ad592
Fan Wu <deepexploring@gmail.com>
2017-10-31 05:36:54 -0700

    Optimal Division

---
+++ 600/553.cpp
@@ -0,0 +1,54 @@
+/*
+    Given a list of positive integers, the adjacent integers will perform the float division. 
+    For example, [2,3,4] -> 2 / 3 / 4.
+
+    However, you can add any number of parenthesis at any position to change the priority of operations. 
+    You should find out how to add parenthesis to get the maximum result, 
+    and return the corresponding expression in string format. Your expression should NOT contain redundant parenthesis.
+
+    Example:
+    Input: [1000,100,10,2]
+    Output: "1000/(100/10/2)"
+    Explanation:
+    1000/(100/10/2) = 1000/((100/10)/2) = 200
+    However, the bold parenthesis in "1000/((100/10)/2)" are redundant, 
+    since they don't influence the operation priority. So you should return "1000/(100/10/2)". 
+
+    Other cases:
+    1000/(100/10)/2 = 50
+    1000/(100/(10/2)) = 50
+    1000/100/10/2 = 0.5
+    1000/100/(10/2) = 2
+    Note:
+
+    The length of the input array is [1, 10].
+    Elements in the given array will be in range [2, 1000].
+    There is only one optimal division for each test case.
+*/
+
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+        if(nums.empty())
+            return "";
+        
+        if(nums.size() == 1)
+            return to_string(nums[0]);
+        
+        string res(to_string(nums[0]));
+        res.append("/");
+        if(nums.size() > 2)
+            res.push_back('(');
+        
+        for(int i = 1; i < nums.size(); ++i)
+        {
+            res.append(to_string(nums[i]));
+            if(i < nums.size() - 1)
+                res.push_back('/');
+        }
+        
+        if(nums.size() > 2)
+            res.push_back(')');
+        return res;
+    }
+};
