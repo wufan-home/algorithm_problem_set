043fac348e8da942fdd50851351a1c07ef9b2221
Fan Wu <deepexploring@gmail.com>
2017-10-31 07:13:34 -0700

    First Bad Version

--- 300/278.cpp
+++ 300/278.cpp
@@ -1,42 +1,33 @@
-// 278.cpp : Defines the entry point for the console application.
-//
+/*
+	You are a product manager and currently leading a team to develop a new product. 
+	Unfortunately, the latest version of your product fails the quality check. 
+	Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
-#include "stdafx.h"
-#include <iostream>
+	Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, 
+	which causes all the following ones to be bad.
 
-using namespace std;
+	You are given an API bool isBadVersion(version) which will return whether version is bad. 
+	Implement a function to find the first bad version. 
+	You should minimize the number of calls to the API.
+*/
 
-bool isBadVersion(int num)
-{
-	if (num >= 2)
-		return true;
+// Forward declaration of isBadVersion API.
+// bool isBadVersion(int version);
 
-	return false;
-}
-
-int firstBadVersion(int n) {
-	if (n == 1)
-		return 1;
-
-	long long start = 1;
-	long long end = n;
-	while (start < end) {
-		if (start == end - 1)
-			return (isBadVersion(start) ? start : end);
-
-		long long mid = (start + end) / 2;
-		if (isBadVersion(static_cast<int>(mid)))
-			end = mid - 1;
-		else
-			start = mid + 1;
-	}
-
-	return start;
-}
-
-int main()
-{
-	cout << firstBadVersion(3) << endl;
-    return 0;
-}
-
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l = 1;
+        int r = n;
+        while(l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if(isBadVersion(mid))
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        
+        return l;
+    }
+};
