4c52f28aa8f1eab4b91f5dce91988eae61b87554
Fan Wu <deepexploring@gmail.com>
2017-11-03 21:57:46 -0700

    Summary Ranges

--- 300/228.cpp
+++ 300/228.cpp
@@ -1,44 +1,44 @@
 /*
-   Given a sorted integer array without duplicates, return the summary of its ranges.
+	Given a sorted integer array without duplicates, return the summary of its ranges.
 
-   For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"]. 
+	Example 1:
+	Input: [0,1,2,4,5,7]
+	Output: ["0->2","4->5","7"]
+	Example 2:
+	Input: [0,2,3,4,6,8,9]
+	Output: ["0","2->4","6","8->9"]
+	
+	Note: This problem is equivalent to the problem to find a consecutive sequence
+	from an ordered array. 
+	
+	If the array is not ordered, the best way is to use the union-find if there is 
+	memory allowance.
 */
 
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-vector<string> summaryRanges(vector<int>& nums) {
-        vector<string> rv;
-	if(nums.empty())
-		return rv;
-
-	for(int i = 0; i < nums.size();)
-	{
-		string interval("");
-		if(i == nums.size() - 1 || nums[i] != nums[i + 1] - 1)
-			interval = to_string(nums[i++]);
-		else
-		{
-			int start = nums[i];
-			for(++i; i < nums.size() && nums[i] == nums[i - 1] + 1; ++i) {}
-			interval = to_string(start) + "->" + to_string(nums[i - 1]);
-		}
-		rv.push_back(interval);
-	}
-
-	return rv;
-}
-
-int main()
-{
-	vector<int> nums({0,1,2,4,5,7});
-	vector<string> rv = summaryRanges(nums);
-	for(int i = 0; i < rv.size(); ++i)
-	{
-		cout << rv[i] << endl;
-	}
-	return 1;
-}
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        nums.push_back(INT_MAX);
+        int size = nums.size();
+        for(int start = 0, end = 0, i = 1; i < size; ++i)
+        {
+            if(nums[i] == nums[end] + 1)
+                ++end;
+            else
+            {
+                res.push_back(to_string(nums[start]));
+                if(end > start)
+                {
+                    res.back().append("->");
+                    res.back().append(to_string(nums[end]));
+                }
+                start = i;
+                end = i;
+            }
+        }
+        nums.pop_back();
+        
+        return res;
+    }
+};
