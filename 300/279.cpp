358f5b7d0d7b71e74ae83d09df41ca18a4c30516
Fan Wu <deepexploring@gmail.com>
2017-11-04 11:52:51 -0700

    Perfect Squares

--- 300/279.cpp
+++ 300/279.cpp
@@ -1,3 +1,41 @@
+/*
+    Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) 
+    which sum to n.
+
+    For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+*/
+
+class Solution {
+public:
+    int numSquares(int n) {
+        if(n < 0)
+            return 0;
+            
+        if(n <= 1)
+            return 1;
+            
+        int root = int(sqrt(n));
+        if(root * root == n)
+            return 1;
+            
+        while(n % 4 == 0)
+            n /= 4;
+            
+        if(n % 8 == 7)
+            return 4;
+            
+        for(int a = 0; a <= root; ++a)
+        {
+            int b = int(sqrt(n - a * a));
+            if(a * a + b * b == n)
+                return 2;
+        }
+        
+        return 3;
+    }
+};
+
+
 #include <iostream>
 #include <stack>
 #include <cmath>
