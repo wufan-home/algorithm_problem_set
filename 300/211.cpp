674a87be3b3628885dd89ec4032446693f4fa8e2
Fan Wu <deepexploring@gmail.com>
2017-11-04 11:49:00 -0700

    Add and Search Word - Data structure design

--- 300/211.cpp
+++ 300/211.cpp
@@ -1,97 +1,103 @@
-#include <iostream>
-#include <string>
-#include <vector>
+/*
+	Design a data structure that supports the following two operations:
 
-using namespace std;
+	void addWord(word)
+	bool search(word)
+	search(word) can search a literal word or a regular expression string containing only letters a-z or .. 
+	A . means it can represent any one letter.
+
+	For example:
+
+	addWord("bad")
+	addWord("dad")
+	addWord("mad")
+	search("pad") -> false
+	search("bad") -> true
+	search(".ad") -> true
+	search("b..") -> true
+	Note:
+	You may assume that all words are consist of lowercase letters a-z.
+*/
 
 class TrieNode
 {
 public:
-	TrieNode() : next(26, NULL), m_stop(false) {}
-	~TrieNode() {}
+    TrieNode()
+    {
+        next = vector<TrieNode *>(26, NULL);
+        tail = false;
+    }
 
-	friend class Trie;
-private:
-	vector<TrieNode*> next;
-	bool m_stop;
+    ~TrieNode()
+    {
+        for(int i = 0; i < 26; ++i)
+            delete next[i];
+    }
+
+    vector<TrieNode *> next;
+    bool tail;
 };
-
-class Trie
-{
-public:
-	Trie() { root = new TrieNode(); }
-	~Trie() {}
-
-	void Add(const string& word);
-	bool Search(const string& word);
-private:
-	TrieNode* SearchRecursively(const string& word, int start, TrieNode *head);
-	TrieNode *root;
-};
-
-void Trie::Add(const string& word)
-{
-	TrieNode *cur = root;
-	for(int i = 0; i < word.size(); ++i)
-	{
-		if(!cur->next[word[i] - 'a'])
-			cur->next[word[i] - 'a'] = new TrieNode;
-
-		cur = cur->next[word[i] - 'a'];
-	}
-	cur->m_stop = true;
-}
-
-bool Trie::Search(const string& word)
-{
-	TrieNode *cur = SearchRecursively(word, 0, root);
-	return (cur && cur->m_stop);
-}
-
-TrieNode* Trie::SearchRecursively(const string& word, int start, TrieNode *head)
-{
-	int s = 0;
-	int e = 25;
-	if(word[start] != '.')
-		s = e = word[start] - 'a';
-
-	TrieNode *result = NULL;
-	for(int i = s; i <= e; ++i)
-	{
-		TrieNode *cur = head->next[i];
-		if(cur && (result = SearchRecursively(word, start + 1, cur)))
-			break;
-	}
-	return result;
-}
 
 class WordDictionary {
 public:
-	WordDictionary() {
-		m_root = new Trie;
-	}
-
-	// Adds a word into the data structure.
-	void addWord(string word) {
-		m_root->Add(word);
-	}
-
-	// Returns if the word is in the data structure. A word could
-	// contain the dot character '.' to represent any one letter.
-	bool search(string word) {
-		return m_root->Search(word);
-	}
-
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        int len = word.size();
+        TrieNode *cur = root;
+        for(int i = 0; i < len; ++i)
+        {
+            if(cur->next[word[i] - 'a'] == NULL)
+                cur->next[word[i] - 'a'] = new TrieNode();
+            
+            if(i == len - 1)
+                cur->next[word[i] - 'a']->tail = true;
+            
+            cur = cur->next[word[i] - 'a'];
+        }
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        return searchAux(word, 0, root);
+    }
+    
 private:
-	Trie *m_root;
+    bool searchAux(const string& word, int start, TrieNode *root)
+    {
+        bool res = false;
+        if(start == word.size())
+            return root->tail;
+        else if(word[start] == '.')
+        {
+            for(int i = 0; i < 26; ++i)
+            {
+                if(root->next[i] && searchAux(word, start + 1, root->next[i]))
+                {
+                    res = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            res = (root->next[word[start] - 'a'] != NULL && 
+                searchAux(word, start + 1, root->next[word[start] - 'a'])); 
+        }
+        
+        return res;
+    }
+    
+    TrieNode *root;
 };
 
-int main()
-{
-	WordDictionary wd;
-	cout << (wd.search("a") ? "True" : "False") << endl;
-	wd.addWord("a");
-	cout << (wd.search("a") ? "True" : "False") << endl;
-	return 0;
-}
-
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * bool param_2 = obj.search(word);
+ */
