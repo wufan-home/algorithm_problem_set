0b82009214c35efe8edf0c615f214eb4132c6059
Fan Wu <deepexploring@gmail.com>
2017-11-04 23:11:25 -0700

    Top K Frequent Words

---
+++ 700/692.cpp
@@ -0,0 +1,51 @@
+/*
+    Given a non-empty list of words, return the k most frequent elements.
+
+    Your answer should be sorted by frequency from highest to lowest. 
+    If two words have the same frequency, then the word with the lower alphabetical order comes first.
+
+    Example 1:
+    Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+    Output: ["i", "love"]
+    Explanation: "i" and "love" are the two most frequent words.
+        Note that "i" comes before "love" due to a lower alphabetical order.
+    Example 2:
+    Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+    Output: ["the", "is", "sunny", "day"]
+    Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+        with the number of occurrence being 4, 3, 2 and 1 respectively.
+    Note:
+    You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+    Input words contain only lowercase letters.
+    Follow up:
+    Try to solve it in O(n log k) time and O(n) extra space.
+*/
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> m;
+        int maxFreq = 0;
+        for(int i = 0; i < words.size(); ++i)
+            maxFreq = max(maxFreq, ++m[words[i]]);
+        
+        vector<vector<string>> freqs(maxFreq + 1, vector<string>());
+        for(auto a : m)
+            freqs[a.second].push_back(a.first);
+        
+        vector<string> res;
+        for(int i = maxFreq; i > 0; --i)
+        {
+            sort(freqs[i].begin(), freqs[i].end());
+            for(auto a : freqs[i])
+            {
+                if(res.size() == k)
+                    break;
+                
+                res.push_back(a);
+            }
+        }
+        
+        return res;
+    }
+};  
