7ceb16f30e74db440493ad1f782d561b9d03e919
Fan Wu <deepexploring@gmail.com>
2017-11-04 17:25:27 -0700

    Ugly Number II

---
+++ 300/264.cpp
@@ -0,0 +1,26 @@
+/*
+    Write a program to find the n-th ugly number.
+    Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+    For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+    Note that 1 is typically treated as an ugly number, and n does not exceed 1690.
+*/
+
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        set<long long> seq;
+        seq.insert(1);
+        int count = 1;
+        while(count < n)
+        {
+            long long num = *(seq.begin());
+            seq.insert(num * 2);
+            seq.insert(num * 3);
+            seq.insert(num * 5);
+            seq.erase(seq.begin());
+            ++count;
+        }
+        
+        return *(seq.begin());
+    }
+};
