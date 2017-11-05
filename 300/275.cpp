3c8843939a8362e779f1b85a40420be5433fa6c4
Fan Wu <deepexploring@gmail.com>
2017-11-04 18:37:37 -0700

    H Index II

--- 300/275.cpp
+++ 300/275.cpp
@@ -1,6 +1,12 @@
 /*
  * Follow up for H-Index: What if the citations array is sorted in ascending order? 
  * Could you optimize your algorithm?
+ 
+    []
+    [0,0,0]
+    [1]
+    [1, 1, 1]
+    [0,1,3,5,6]
  */
 
 class Solution {
