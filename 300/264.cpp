c0fa42b8828d76f18ea26e2a495ed6f218ad07a9
Fan Wu <deepexploring@gmail.com>
2017-11-04 17:32:24 -0700

    Ugly Number II

--- 300/264.cpp
+++ 300/264.cpp
@@ -5,6 +5,28 @@
     Note that 1 is typically treated as an ugly number, and n does not exceed 1690.
 */
 
+// Indices of three lists. This is a better way to solve the questions.
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> res(1, 1);
+        int i2 = 0, i3 = 0, i5 = 0;
+        while (res.size() < n) {
+            int m2 = res[i2] * 2, m3 = res[i3] * 3, m5 = res[i5] * 5;
+            int mn = min(m2, min(m3, m5));
+            if (mn == m2) ++i2;
+            if (mn == m3) ++i3;
+            if (mn == m5) ++i5;
+            res.push_back(mn);
+        }
+        return res.back();
+    }
+};
+
+// Use the structure of a binary search tree.
+// It can sort the elements autometically.
+// Insert the 2, 3, 5 time the first element and remove this one.
+// After n - 1 time, the first element is the result.
 class Solution {
 public:
     int nthUglyNumber(int n) {
