ba5744f14cf40cfff8b1dea32dd82a3b50e758b4
Fan Wu <deepexploring@gmail.com>
2017-12-03 12:29:52 -0800

    Find Minimum in Rotated Sorted Array II

--- 200/154.cpp
+++ 200/154.cpp
@@ -20,26 +20,19 @@ public:
         while(l < r)
         {
             if(nums[l] == nums[r])
-            {
-                for(--r; r > l && nums[r] == nums[r + 1]; --r) {}
-                continue;
-            }
+                for(; r > l && nums[r] == nums[l]; --r) {}
+            
+            if(r == l)
+                break;
             
             int mid = l + (r - l) / 2;
-            if(nums[mid] <= nums[r])
-            {
-                if(mid == l || nums[mid] >= nums[mid - 1])
-                    r = mid - 1;
-                else
-                    l = mid;
-            }
+            for(; mid > l && nums[mid] == nums[mid - 1]; --mid) {}
+            if(nums[mid] < nums[mid + 1] && (mid == l || nums[mid] < nums[mid - 1]))
+                return nums[mid];
+            else if(nums[mid] > nums[r])
+                l = mid + 1;
             else
-            {
-                if(mid == l)
-                    l = mid + 1;
-                else
-                    l = mid;
-            }
+                r = mid;
         }
         
         return nums[l];
