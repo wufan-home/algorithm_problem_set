5ea8534dd03e5286392ba802ec706ff30fd0ad58
Fan Wu <deepexploring@gmail.com>
2017-12-03 13:42:51 -0800

    Find Peak Element

--- 200/162.cpp
+++ 200/162.cpp
@@ -16,30 +16,19 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        if(nums.empty())
-            return INT_MIN;
-
-        int n = nums.size();
-        if(n == 1)
-            return 0;
-            
-        if(nums[0] > nums[1])
-            return 0;
-        
-        if(nums[n - 2] < nums[n - 1])
-            return nums.size() - 1;
-            
-        for(int l = 0, r = n - 1; l < r;)
+        int l = 0;
+        int r = nums.size() - 1;
+        while(l < r)
         {
             int mid = l + (r - l) / 2;
-            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            if(nums[mid] > nums[mid + 1] && (mid == 0 || nums[mid] > nums[mid - 1]))
                 return mid;
-            else if(nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1])
+            else if(nums[mid] < nums[mid + 1])
                 l = mid + 1;
             else
                 r = mid;
         }
         
-        return INT_MIN;
+        return l;
     }
 };
