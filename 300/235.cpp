df0c9b0e2a72e20a65a85805a76fd23cc9f8d373
Fan Wu <deepexploring@gmail.com>
2017-12-10 19:32:39 -0800

    Lowest Common Ancestor of a Binary Search Tree

--- 300/235.cpp
+++ 300/235.cpp
@@ -17,6 +17,37 @@
     according to the LCA definition.
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// Better method.
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val > q->val)
+            swap(p, q);
+        
+        cout << root->val << endl;
+        
+        if(root->val >= p->val && root->val <= q->val)
+            return root;
+        else if(root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
+        else
+            return lowestCommonAncestor(root->right, p, q);
+    }
+};
+
+// General method.
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
