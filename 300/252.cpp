6dd56a74b9e883e89303e03812547874629f8edf
Fan Wu <deepexploring@gmail.com>
2017-12-10 18:05:43 -0800

    Meeting Rooms

--- 300/252.cpp
+++ 300/252.cpp
@@ -6,24 +6,6 @@
   return false.
 */
 
-static bool Compare(const Interval& l, const Interval& r)
-{
-	return l.start < r.start || (l.start == r.start && l.end <= r.end);
-}
-
-bool canAttendMeetings(vector<Interval>& intervals) 
-{
-	sort(intervals.begin(), intervals.end(), Compare);
-	for(int i = 0; i < (int)intervals.size() - 1; ++i)
-        {
-		if(intervals[i].end > intervals[i + 1].start)
-			return false;
-        }
-        
-        return true;
-}
-
-// Using the quick sort.
 /**
  * Definition for an interval.
  * struct Interval {
@@ -36,43 +18,13 @@ bool canAttendMeetings(vector<Interval>& intervals)
 class Solution {
 public:
     bool canAttendMeetings(vector<Interval>& intervals) {
-        if(intervals.size() <= 1)
-            return true;
-            
-        return sortedIntervalsWithCheckingIntersection(intervals, 0, intervals.size() - 1);
-    }
-    
-private:
-    bool sortedIntervalsWithCheckingIntersection(vector<Interval>& intervals, int start, int end)
-    {
-        if(start >= end)
-            return true;
-
-        int write = start - 1;
-        for(int i = start; i < end; ++i)
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b) { return a.start < b.start; });
+        for(int cur = 1; cur < intervals.size(); ++cur)
         {
-            if(checkTwoIntervalsIntersection(intervals[i], intervals[end]))
+            if(intervals[cur].start < intervals[cur - 1].end)
                 return false;
-                
-            if(intervals[i].end <= intervals[end].start)
-                swapIntervals(intervals[++write], intervals[i]);
         }
-        swapIntervals(intervals[++write], intervals[end]);
-
-        return sortedIntervalsWithCheckingIntersection(intervals, start, write - 1) && 
-                sortedIntervalsWithCheckingIntersection(intervals, write + 1, end);
-    }
-    
-    bool checkTwoIntervalsIntersection(const Interval &l, const Interval& r)
-    {
-        return (l.start == r.start && l.end == r.end) || 
-               (l.start > r.start && l.start < r.end) || (l.end > r.start && l.end < r.end) || 
-               (r.start > l.start && r.start < l.end) || (r.end > l.start && r.end < l.end);
-    }
-    
-    void swapIntervals(Interval &l, Interval &r)
-    {
-        swap(l.start, r.start);
-        swap(l.end, r.end);
+        
+        return true;
     }
 };
