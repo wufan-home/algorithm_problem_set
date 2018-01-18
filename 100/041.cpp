a6937e2ac29fb643a4f29e948172df4cf009481b
Fan Wu <deepexploring@gmail.com>
2018-01-18 00:12:31 -0800

    First Missing Positive

--- 100/041.cpp
+++ 100/041.cpp
@@ -12,14 +12,9 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         for(int i = 1; i <= nums.size(); ++i)
-        {
-            if(nums[i - 1] <= 0 || nums[i - 1] > nums.size())
-                nums[i - 1] = 0;
-            
-            while(nums[i - 1] > 0 && nums[i - 1] != i && nums[nums[i - 1] - 1] != nums[i - 1])
-            {
+        {   
+            while(nums[i - 1] > 0 && nums[i - 1] <= nums.size() && nums[i - 1] != i && nums[nums[i - 1] - 1] != nums[i - 1])
                 swap(nums[i - 1], nums[nums[i - 1] - 1]);
-            }
         }
         
         for(int i = 0; i < nums.size(); ++i)
