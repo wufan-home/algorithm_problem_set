6fd20be91f936ee6780dace8944e7daad03bc431
Fan Wu <deepexploring@gmail.com>
2020-09-13 09:30:50 -0700

    Create 559.cpp

---
+++ 600/559.cpp
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == NULL)
+            return 0;
+        
+        int max_child_depth = 0;
+        for (int i = 0; i< root->children.size(); ++i) {
+            max_child_depth = max (max_child_depth, maxDepth(root->children[i]));
+        }
+        
+        return 1 + max_child_depth;
+    }
+};
