755ec3ecc5d56465b29ce2976f7125daec3b7173
Fan Wu <deepexploring@gmail.com>
2021-01-09 17:34:31 -0800

    Update 973.cpp

--- 1000/973.cpp
+++ 1000/973.cpp
@@ -38,3 +38,51 @@ private:
         }
     };
 };
+
+// Quick Selection
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        int size = points.size();
+        
+        vector<pair<int, int>> cache(size, make_pair(0, 0));
+        for (int i = 0; i < size; ++i) {
+            cache[i] = make_pair(i, points[i][0] * points[i][0] + points[i][1] * points[i][1]);
+        }
+        
+        int start = 0;
+        int end = size - 1;
+        while (start < end) {
+            int middle = end;
+            int distance = cache[start].second;
+            for (int i = start + 1; i <= middle;) {
+                if (cache[i].second > distance) {
+                    swap(cache[i], cache[middle--]);
+                }
+                else {
+                    ++i;
+                }
+            }
+            
+            swap(cache[start], middle > start ? cache[middle] : cache[start]);
+            
+            if (middle - start == K) {
+                break;
+            }
+            else if (middle > K) {
+                end = middle - 1;
+            }
+            else {
+                start = middle + 1;
+            }
+        }
+        
+        vector<vector<int>> res(K, vector<int>(2, -1));
+        for (int i = 0; i < K; ++i) {
+            res[i] = points[cache[i].first];
+        }
+        
+        return res;
+    }
+};
