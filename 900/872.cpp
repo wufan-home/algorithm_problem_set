96bf7d5867100a8295c861d4cca1fa2bc9e26efc
Fan Wu <deepexploring@gmail.com>
2021-01-18 10:01:22 -0800

    Create 872.cpp

---
+++ 900/872.cpp
@@ -0,0 +1,105 @@
+/*
+    Consider all the leaves of a binary tree, from left to right order, the values of those leaves form a leaf value sequence.
+
+    For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+
+    Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+
+    Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+
+
+
+    Example 1:
+
+    Input: root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+    Output: true
+
+    Example 2:
+
+    Input: root1 = [1], root2 = [1]
+    Output: true
+
+    Example 3:
+
+    Input: root1 = [1], root2 = [2]
+    Output: false
+
+    Example 4:
+
+    Input: root1 = [1,2], root2 = [2,2]
+    Output: true
+
+    Example 5:
+
+    Input: root1 = [1,2,3], root2 = [1,3,2]
+    Output: false
+
+
+
+    Constraints:
+
+        The number of nodes in each tree will be in the range [1, 200].
+        Both of the given trees will have values in the range [0, 200].
+
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int> leaves;
+        getLeaves(root1, leaves);
+        bool flag = true;
+        int index = 0;
+        checkSameLeaves(root2, leaves, index, flag);
+        return flag && index == leaves.size();
+    }
+    
+private:
+    void getLeaves(TreeNode* root, vector<int>& leaves) {
+        if (root->left == NULL && root->right == NULL) {
+            leaves.push_back(root->val);
+            return;
+        }
+        
+        if (root->left) {
+            getLeaves(root->left, leaves);
+        }
+        
+        if (root->right) {
+            getLeaves(root->right, leaves);
+        }
+    }
+    
+    void checkSameLeaves(TreeNode* root, const vector<int>& leaves, int& index, bool& flag) {
+        if (root->left == NULL && root->right == NULL) {
+            if (leaves[index++] == root->val) {
+                flag = true;
+            }
+            else {
+                flag = false;
+            }
+            
+            return;
+        }
+        
+        if (flag && root->left) {
+            checkSameLeaves(root->left, leaves, index, flag);
+        }
+        
+        if (flag && root->right) {
+            checkSameLeaves(root->right, leaves, index, flag);
+        }
+    }
+};
