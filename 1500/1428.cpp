748deeafcf2472dd84a1e4b6ed030dbdfbfa0af4
Fan Wu <deepexploring@gmail.com>
2021-03-11 20:53:21 -0800

    Create 1428.cpp

---
+++ 1500/1428.cpp
@@ -0,0 +1,57 @@
+/*
+    (This problem is an interactive problem.)
+
+    A row-sorted binary matrix means that all elements are 0 or 1 and each row of the matrix is sorted in non-decreasing order.
+
+    Given a row-sorted binary matrix binaryMatrix, return the index (0-indexed) of the leftmost column with a 1 in it. If such an index does not exist, return -1.
+
+    You can't access the Binary Matrix directly. You may only access the matrix using a BinaryMatrix interface:
+
+        BinaryMatrix.get(row, col) returns the element of the matrix at index (row, col) (0-indexed).
+        BinaryMatrix.dimensions() returns the dimensions of the matrix as a list of 2 elements [rows, cols], which means the matrix is rows x cols.
+
+    Submissions making more than 1000 calls to BinaryMatrix.get will be judged Wrong Answer. Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+    For custom testing purposes, the input will be the entire binary matrix mat. You will not have access to the binary matrix directly.
+    
+    [[0,0],[1,1]]
+    [[1]]
+    [[0]]
+    [[0, 0],[0, 0]]
+    [[0,0,0,1],[0,0,1,1],[0,1,1,1]]
+    [[0,0],[0,1]]
+    
+    Expect:
+    0
+    0
+    -1
+    -1
+    1
+    1
+*/
+
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class BinaryMatrix {
+ *   public:
+ *     int get(int row, int col);
+ *     vector<int> dimensions();
+ * };
+ */
+
+class Solution {
+public:
+    int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        int row = binaryMatrix.dimensions()[0];
+        int col = binaryMatrix.dimensions()[1];
+        
+        int res = col;
+        for (int i = 0, c = col - 1; i < row; ++i) {
+            for (;c >=0 && binaryMatrix.get(i, c) == 1; --c) {}
+            res = c + 1;
+        }
+        
+        return res == col ? -1 : res;
+    }
+};
