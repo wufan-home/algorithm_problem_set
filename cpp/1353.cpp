f60637fa7cbcd2034a78169bad9b54101446682b
Fan Wu <deepexploring@gmail.com>
2021-04-04 12:37:41 -0700

    Update 1353.cpp

--- cpp/1353.cpp
+++ cpp/1353.cpp
@@ -76,3 +76,28 @@ public:
         return res;
     }
 };
+
+/*
+    This solution is from the link: https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/discuss/510263/JavaC%2B%2BPython-Priority-Queue
+*/
+
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& A) {
+        priority_queue <int, vector<int>, greater<int>> pq;
+        sort(A.begin(), A.end());
+        int i = 0, res = 0, n = A.size();
+        for (int d = 1; d <= 100000; ++d) {
+            while (pq.size() && pq.top() < d)
+                pq.pop();
+            while (i < n && A[i][0] == d)
+                pq.push(A[i++][1]);
+            if (pq.size()) {
+                pq.pop();
+                ++res;
+            }
+        }
+        return res;
+    }
+};
+
