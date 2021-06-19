f2d86fe0d53a838bc6f70c7ab98cfd1b8ae28728
Fan Wu <deepexploring@gmail.com>
2021-06-19 16:37:48 -0700

    Create 1004.cpp

---
+++ cpp/1004.cpp
@@ -0,0 +1,40 @@
+/*
+    Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+    Example 1:
+
+    Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+    Output: 6
+    Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+    Example 2:
+
+    Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+    Output: 10
+    Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+    Constraints:
+
+        1 <= nums.length <= 105
+        nums[i] is either 0 or 1.
+        0 <= k <= nums.length
+*/
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int size = nums.size();
+        int countOne = 0;
+        int ans = 0;
+        for (int l = 0, r = 0; r < size; ++r) {
+            countOne += nums[r] == 1 ? 1 : 0;
+            while (r - l + 1 - countOne > k) {
+                countOne -= nums[l++] == 1 ? 1 : 0;
+            }
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+};
