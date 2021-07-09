2f53aa3d6e8e9c80c50802a87a4e2b1eb96c7173
Fan Wu <deepexploring@gmail.com>
2021-07-08 23:54:06 -0700

    Update 0215.cpp

--- cpp/0215.cpp
+++ cpp/0215.cpp
@@ -27,3 +27,37 @@ int findKthLargest(vector<int>& nums, int k) {
     	}
     	return nums[s];
 }
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        std::srand(std::time(nullptr));
+        int l = 0;
+        int r = nums.size();
+        int ans = l;
+        while (l < r) {
+            ans = l;
+            swap(nums[l + std::rand() % (r - l)], nums[r - 1]);
+            for (int i = l; i < r - 1; ++i) {
+                if (nums[i] <= nums[r - 1]) {
+                    swap(nums[ans++], nums[i]);
+                }
+            }
+            swap(nums[ans], nums[r - 1]);
+            
+            if (k == (r - ans <= k ? r - ans : 0)) {
+                break;
+            }
+
+            if (r - ans <= k) {
+                k -= r - ans;
+                r = ans;
+            }
+            else {
+                l = ans;
+            }
+        }
+        
+        return nums[ans];
+    }
+};
