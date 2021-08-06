d4e49e2dc5df9324d57241bf4065400f85fcdc33
Fan Wu <deepexploring@gmail.com>
2021-08-06 11:52:31 -0700

    Update 0127.cpp

--- cpp/0127.cpp
+++ cpp/0127.cpp
@@ -1,96 +1,71 @@
 /*
-  Given two words (beginWord and endWord), and a dictionary's word list, 
-  find the length of shortest transformation sequence from beginWord to endWord, such that:
-  Only one letter can be changed at a time
-  Each intermediate word must exist in the word list
-  For example,
-  Given:
-  beginWord = "hit"
-  endWord = "cog"
-  wordList = ["hot","dot","dog","lot","log"]
-  As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
-  return its length 5.
+    A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
+
+    Every adjacent pair of words differs by a single letter.
+    Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+    sk == endWord
+    Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+
+
+    Example 1:
+
+    Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+    Output: 5
+    Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+    Example 2:
+
+    Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+    Output: 0
+    Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+
+
+    Constraints:
+
+    1 <= beginWord.length <= 10
+    endWord.length == beginWord.length
+    1 <= wordList.length <= 5000
+    wordList[i].length == beginWord.length
+    beginWord, endWord, and wordList[i] consist of lowercase English letters.
+    beginWord != endWord
+    All the words in wordList are unique.
 */
 
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        if(find(wordList.begin(), wordList.end(), endWord) == wordList.end())
-            return 0;
-
-        unordered_set<string> dict;
-        for(int i = 0; i < wordList.size(); ++i)
-            dict.insert(wordList[i]);
-        
-        beginToWord[beginWord] = 1;
-        queueFromBegin.push(beginWord);
-        
-        wordToEnd[endWord] = 1;
-        queueFromEnd.push(endWord);
-
-        while(!queueFromBegin.empty() && !queueFromEnd.empty())
-        {
-            string word = queueFromBegin.front();
-            queueFromBegin.pop();
-            int length = searchAdjacentWords(word, dict, true);
-            if(length > 0)
-                return length;
-                
-            word = queueFromEnd.front();
-            queueFromEnd.pop();
-            length = searchAdjacentWords(word, dict, false);
-            if(length > 0)
-                return length;
-        }
-        
-        return 0;
-    }
-private:
-    int searchAdjacentWords(const string& word, const unordered_set<string>& dict, bool forward)
-    {
-        string cur = word;
-        int len = cur.size();
-        for(int i = 0; i < len; ++i)
-        {
-            for(char c = 'a'; c <= 'z'; ++c)
-            {
-                cur[i] = c;
-                if(dict.find(cur) == dict.end())
-                    continue;
-
-                if(forward)
-                {
-                    if(wordToEnd.find(cur) != wordToEnd.end())
-                        return beginToWord[word] + wordToEnd[cur];
-                    
-                    if(beginToWord.find(cur) != beginToWord.end())
-                        continue;
-
-                    beginToWord[cur] = beginToWord[word] + 1;
-                    queueFromBegin.push(cur);
-                }
-                else
-                {
-                    if(beginToWord.find(cur) != beginToWord.end())
-                        return beginToWord[cur] + wordToEnd[word];
-                    
-                    if(wordToEnd.find(cur) != wordToEnd.end())
-                        continue;
-
-                    wordToEnd[cur] = wordToEnd[word] + 1;
-                    queueFromEnd.push(cur);
-                }
+        int len = beginWord.size();
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        queue<string> q;
+        q.push(beginWord);
+        unordered_set<string> visited;
+        visited.insert(beginWord);
+        int ans = 1;
+        bool found = false;
+        for (int level = q.size();!q.empty();) {
+            if (q.front() == endWord) {
+                found = true;
+                break;
             }
-            
-            cur = word;
+            string cur = q.front();
+            q.pop();
+            for (int i = 0; i < len; ++i) {
+                char origin = cur[i];
+                for (int j = 0; j < 26; ++j) {
+                    cur[i] = 'a' + j;
+                    
+                    if (visited.count(cur) == 0 && dict.count(cur) > 0) {
+                        visited.insert(cur);
+                        q.push(cur);
+                    }
+                }
+                cur[i] = origin;
+            }
+            if (--level == 0) {
+                ++ans;
+                level = q.size();
+            }
         }
-        
-        return 0;
+        return !found ? 0 : ans;
     }
-
-    unordered_map<string, int> beginToWord;
-    queue<string> queueFromBegin;
-    
-    unordered_map<string, int> wordToEnd;
-    queue<string> queueFromEnd;
 };
