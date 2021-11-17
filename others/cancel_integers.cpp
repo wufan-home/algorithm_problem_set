82146a55e5d99fd18c7e7fbb81fc508e5b965e97
Fan Wu <deepexploring@gmail.com>
2021-11-17 14:16:11 -0800

    Create cancel_integers.cpp

---
+++ others/cancel_integers.cpp
@@ -0,0 +1,14 @@
+/*
+    Give an sequence of integers. If there are three or more consecutive same elements in it, remove all of them from the sequence. 
+    
+    Repeat this action until there are no such segments found. Print the final form of the sequence.
+
+    Eg. [1] -> [1]
+    Eg. [1,2] -> [1,2]
+    Eg. [1,1] -> [1,1]
+    Eg. [1,1,1] -> []
+    Eg. [1,2,2,2,1] -> [1,1]
+    Eg. [1,1,2,2,2,1] -> []
+
+    Assume: 1 <= sequence size <= 10000.
+*/
